9414f083ac7da039681745a59a872d1b51cccb16
Romaric Philogene <evoxmusic@gmail.com>
2020-06-30 21:52:29 +0200

    chore: Node support multiple instance types

--- src/cloud_provider/aws/kubernetes/node.rs
+++ src/cloud_provider/aws/kubernetes/node.rs
@@ -3,6 +3,7 @@ use crate::cloud_provider::KubernetesNode;
 pub struct Node {
     total_cpu: u8,
     total_memory_in_gib: u16,
+    instance_types_table: [(u8, u16, &'static str); 6],
 }
 
 impl Node {
@@ -11,9 +12,19 @@ impl Node {
     /// BUT total_cpu = 1 and total_memory_in_gib = 3 does not have an existing instance - so we will pick the upper closest,
     /// which is `t2.medium` with 2 cpu and 4 GiB
     pub fn new(total_cpu: u8, total_memory_in_gib: u16) -> Self {
+        let instance_types_table = [
+            (1, 1, "t2.micro"),
+            (1, 2, "t2.small"),
+            (2, 4, "t2.medium"),
+            (2, 8, "t2.large"),
+            (4, 16, "t2.xlarge"),
+            (8, 32, "t2.2xlarge"),
+        ];
+
         Node {
             total_cpu,
             total_memory_in_gib,
+            instance_types_table,
         }
     }
 }
@@ -28,11 +39,45 @@ impl KubernetesNode for Node {
     }
 
     fn instance_type(&self) -> &str {
-        // FIXME: return the right instance type
-        if self.total_cpu == 1 {
-            "t2.small"
-        } else {
-            "t2.medium"
+        if self.total_cpu == 0 || self.total_memory_in_gib == 0 {
+            let (_, _, instance_type) = self.instance_types_table.first().unwrap();
+            return instance_type;
         }
+
+        for (_cpu, mem, instance_type) in self.instance_types_table.iter() {
+            if self.total_memory_in_gib <= *mem {
+                return instance_type;
+            }
+        }
+
+        let (_, _, instance_type) = self.instance_types_table.last().unwrap();
+        return instance_type;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::cloud_provider::aws::kubernetes::node::Node;
+    use crate::cloud_provider::KubernetesNode;
+
+    #[test]
+    fn test_instance_types() {
+        assert_eq!(Node::new(0, 0).instance_type(), "t2.micro");
+        assert_eq!(Node::new(1, 0).instance_type(), "t2.micro");
+        assert_eq!(Node::new(0, 1).instance_type(), "t2.micro");
+        assert_eq!(Node::new(1, 1).instance_type(), "t2.micro");
+        assert_eq!(Node::new(1, 2).instance_type(), "t2.small");
+        assert_eq!(Node::new(2, 4).instance_type(), "t2.medium");
+        assert_eq!(Node::new(2, 5).instance_type(), "t2.large");
+        assert_eq!(Node::new(1, 6).instance_type(), "t2.large");
+        assert_eq!(Node::new(1, 7).instance_type(), "t2.large");
+        assert_eq!(Node::new(2, 8).instance_type(), "t2.large");
+        assert_eq!(Node::new(3, 8).instance_type(), "t2.large");
+        assert_eq!(Node::new(3, 10).instance_type(), "t2.xlarge");
+        assert_eq!(Node::new(3, 12).instance_type(), "t2.xlarge");
+        assert_eq!(Node::new(4, 16).instance_type(), "t2.xlarge");
+        assert_eq!(Node::new(4, 17).instance_type(), "t2.2xlarge");
+        assert_eq!(Node::new(8, 32).instance_type(), "t2.2xlarge");
+        assert_eq!(Node::new(16, 64).instance_type(), "t2.2xlarge");
     }
 }
