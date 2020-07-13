3a31ad1a7c8ccdde631caabf9fcef825bccb7f33
Pierre Mavro <deimos@deimos.fr>
2020-07-13 11:47:01 +0200

    fix: fluentd AWS connection issue

--- lib/aws/bootstrap/helm-fluent-bit.tf
+++ lib/aws/bootstrap/helm-fluent-bit.tf
@@ -9,17 +9,11 @@ resource "aws_iam_access_key" "fluent-bit" {
 resource "helm_release" "fluent-bit" {
   name = "fluent-bit"
   chart = "../../../lib/common/charts/fluent-bit"
-  namespace = "prometheus"
+  namespace = "logging"
+  create_namespace = "true"
   atomic = true
   max_history = 50
-
-  values = [file("chart_values/fluent-bit.yaml")]
-
-  # Override image to support AWS auth
-  set {
-    name = "image.fluent_bit.tag"
-    value = "1.4.6"
-  }
+  force_update = true
 
   # Enable Prometheus exporter
   set {
@@ -33,40 +27,93 @@ resource "helm_release" "fluent-bit" {
   }
 
   # Set AWS auth
+
   set {
-    name = "backend.es.host"
+    name = "awsEsProxy.host"
     value = aws_elasticsearch_domain.qovery-k8s-logs.endpoint
   }
 
   set {
-    name = "backend.es.port"
-    value = "443"
+    name = "awsEsProxy.port"
+    value = "9200"
   }
 
   set {
-    name = "backend.es.aws_auth"
-    value = "On"
-  }
-
-  set {
-    name = "backend.es.aws_region"
-    value = var.region
-  }
-
-  set {
-    name = "backend.es.tls"
-    value = "On"
-  }
-
-  set {
-    name = "AWS_ACCESS_KEY_ID"
+    name = "awsEsProxy.accessKey"
     value = aws_iam_access_key.fluent-bit.id
   }
 
   set {
-    name = "AWS_SECRET_ACCESS_KEY"
+    name = "awsEsProxy.secretKey"
     value = aws_iam_access_key.fluent-bit.secret
   }
+
+  set {
+    name = "backend.type"
+    value = "es"
+  }
+
+  set {
+    name = "backend.es.host"
+    value = "localhost"
+  }
+
+  // AWS direct authentication is not production ready
+//  set {
+//    name = "image.fluent_bit.tag"
+//    value = "1.4.6"
+//  }
+//  set {
+//    name = "backend.es.host"
+//    value = aws_elasticsearch_domain.qovery-k8s-logs.endpoint
+//  }
+//
+//  set {
+//    name = "backend.es.port"
+//    value = "443"
+//  }
+//
+//  set {
+//    name = "backend.es.aws_auth"
+//    value = "on"
+//  }
+//
+//  set {
+//    name = "backend.es.aws_region"
+//    value = var.region
+//  }
+//
+//  set {
+//    name = "backend.es.tls"
+//    value = "on"
+//  }
+//
+//  set {
+//    name = "env[0].name"
+//    value = "AWS_ACCESS_KEY_ID"
+//  }
+//  set {
+//    name = "env[0].value"
+//    value = aws_iam_access_key.fluent-bit.id
+//  }
+//
+//  set {
+//    name = "env[1].name"
+//    value = "AWS_SECRET_ACCESS_KEY"
+//  }
+//  set {
+//    name = "env[1].value"
+//    value = aws_iam_access_key.fluent-bit.secret
+//  }
+//
+//  set {
+//    name = "env[2].name"
+//    value = "AWS_SESSION_TOKEN"
+//  }
+//  set {
+//    name = "env[2].value"
+//    value = aws_iam_access_key.fluent-bit.secret
+//  }
 
   depends_on = [aws_eks_cluster.eks_cluster, helm_release.prometheus-operator]
 }
