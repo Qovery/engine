5d9adaaa8596d0fe1c298c81ba97f69bbac898f2
Romaric Philogene <evoxmusic@gmail.com>
2020-07-31 08:22:38 +0200

    fix: test init bug

--- qovery-engine/tests/aws_kubernetes.rs
+++ qovery-engine/tests/aws_kubernetes.rs
@@ -3,14 +3,14 @@ extern crate test_utilities;
 use qovery_engine::cloud_provider::aws::kubernetes::node::Node;
 use qovery_engine::cloud_provider::aws::kubernetes::EKS;
 use qovery_engine::cloud_provider::aws::AWS;
-use qovery_engine::cloud_provider::kubernetes::Kubernetes;
+use qovery_engine::cloud_provider::kubernetes::{Kubernetes, KubernetesError};
 use qovery_engine::cloud_provider::CloudProvider;
 use qovery_engine::transaction::TransactionResult;
 use std::borrow::Borrow;
 use test_utilities::AWS_KUBERNETES_VERSION;
 
 #[test]
-fn create_eks_cluster_in_us_east_2() {
+fn create_eks_cluster_in_us_east_2() -> Result<(), KubernetesError> {
     test_utilities::init();
 
     let context = test_utilities::context();
@@ -32,17 +32,19 @@ fn create_eks_cluster_in_us_east_2() {
         nodes,
     );
 
-    tx.create_kubernetes(&kubernetes);
+    let _ = tx.create_kubernetes(&kubernetes)?;
 
-    match tx.commit() {
+    let _ = match tx.commit() {
         TransactionResult::Ok => assert!(true),
         TransactionResult::Rollback(_) => assert!(false),
         TransactionResult::UnrecoverableError(_, _) => assert!(false),
     };
+
+    Ok(())
 }
 
 #[test]
-fn create_eks_cluster_in_eu_west_3() {
+fn create_eks_cluster_in_eu_west_3() -> Result<(), KubernetesError> {
     test_utilities::init();
 
     let context = test_utilities::context();
@@ -64,17 +66,19 @@ fn create_eks_cluster_in_eu_west_3() {
         nodes,
     );
 
-    tx.create_kubernetes(&kubernetes);
+    let _ = tx.create_kubernetes(&kubernetes)?;
 
-    match tx.commit() {
+    let _ = match tx.commit() {
         TransactionResult::Ok => assert!(true),
         TransactionResult::Rollback(_) => assert!(false),
         TransactionResult::UnrecoverableError(_, _) => assert!(false),
     };
+
+    Ok(())
 }
 
 #[test]
-fn delete_eks_cluster_in_us_east_2() {
+fn delete_eks_cluster_in_us_east_2() -> Result<(), KubernetesError> {
     test_utilities::init();
 
     let context = test_utilities::context();
@@ -96,17 +100,19 @@ fn delete_eks_cluster_in_us_east_2() {
         nodes,
     );
 
-    tx.delete_kubernetes(&kubernetes);
+    let _ = tx.create_kubernetes(&kubernetes)?;
 
-    match tx.commit() {
+    let _ = match tx.commit() {
         TransactionResult::Ok => assert!(true),
         TransactionResult::Rollback(_) => assert!(false),
         TransactionResult::UnrecoverableError(_, _) => assert!(false),
     };
+
+    Ok(())
 }
 
 #[test]
-fn delete_eks_cluster_in_eu_west_3() {
+fn delete_eks_cluster_in_eu_west_3() -> Result<(), KubernetesError> {
     test_utilities::init();
 
     let context = test_utilities::context();
@@ -128,11 +134,13 @@ fn delete_eks_cluster_in_eu_west_3() {
         nodes,
     );
 
-    tx.delete_kubernetes(&kubernetes);
+    let _ = tx.create_kubernetes(&kubernetes)?;
 
-    match tx.commit() {
+    let _ = match tx.commit() {
         TransactionResult::Ok => assert!(true),
         TransactionResult::Rollback(_) => assert!(false),
         TransactionResult::UnrecoverableError(_, _) => assert!(false),
     };
+
+    Ok(())
 }
