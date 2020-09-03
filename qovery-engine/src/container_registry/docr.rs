924e95425a48da6be380f1e74d515696c179d2e6
maathor <marc.anthony.richard@gmail.com>
2020-09-03 19:16:00 +0200

    implement the push on container registry

--- qovery-engine/src/container_registry/docr.rs
+++ qovery-engine/src/container_registry/docr.rs
@@ -2,6 +2,8 @@ use crate::container_registry::{
     ContainerRegistry, ContainerRegistryError, Kind, PushError, PushResult,
 };
 use crate::models::{Context, ProgressListener};
+use crate::cmd;
+use crate::cmd::CmdError;
 use std::rc::Rc;
 use crate::build_platform::Image;
 
@@ -66,8 +68,56 @@ impl ContainerRegistry for DOCR {
         unimplemented!()
     }
 
+    // https://www.digitalocean.com/docs/images/container-registry/how-to/use-registry-docker-kubernetes/
     fn push(&self, image: &Image, force_push: bool) -> Result<PushResult, PushError> {
-        unimplemented!()
+        match cmd::exec(
+            "docker",
+            vec![
+                "login",
+                "-u",
+                self.login.as_str(),
+                "-p",
+                self.password.as_str(),
+            ],
+        ) {
+            Err(err) => match err {
+                CmdError::Exec(exit_status) => return Err(PushError::CredentialsError),
+                CmdError::Io(err) => panic!(err),
+                CmdError::Unexpected(err) => panic!(err),
+            },
+            _ => {}
+        };
+
+        let dest = format!("{}/{}", self.login.as_str(), image.name_with_tag().as_str());
+        match cmd::exec(
+            "docker",
+            vec![
+                "tag",
+                dest.as_str(),
+                format!("{}/{}", self.login.as_str(), dest.as_str()).as_str(),
+            ],
+        ) {
+            Err(err) => match err {
+                CmdError::Exec(exit_status) => return Err(PushError::ImageTagFailed),
+                CmdError::Io(err) => panic!(err),
+                CmdError::Unexpected(err) => panic!(err),
+            },
+            _ => {}
+        };
+
+        match cmd::exec("docker", vec!["push", dest.as_str()]) {
+            Err(err) => match err {
+                CmdError::Exec(exit_status) => return Err(PushError::ImagePushFailed),
+                CmdError::Io(err) => panic!(err),
+                CmdError::Unexpected(err) => panic!(err),
+            },
+            _ => {}
+        };
+
+        let mut image = image.clone();
+        image.registry_url = Some(dest);
+
+        Ok(PushResult { image })
     }
 
     fn push_error(&self, image: &Image) -> Result<PushResult, PushError> {
