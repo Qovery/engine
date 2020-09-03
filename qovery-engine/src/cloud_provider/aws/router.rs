2aa75526b660c23bd7a50b1c6d6455ab3b703335
Pierre Mavro <deimos@deimos.fr>
2020-09-03 14:57:41 +0200

    feat: temporary remove DNS check for deployed apps

--- qovery-engine/src/cloud_provider/aws/router.rs
+++ qovery-engine/src/cloud_provider/aws/router.rs
@@ -17,12 +17,12 @@ use crate::cloud_provider::{CloudProvider, DeploymentTarget};
 use crate::cmd::CmdError;
 use crate::constants::{AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY};
 use crate::models::Context;
+use crate::transaction::{ActionContext, Kind};
 use dns_lookup::lookup_host;
 use itertools::enumerate;
 use retry::delay::{Exponential, Fibonacci};
 use retry::OperationResult;
 use serde::{Deserialize, Serialize};
-use crate::transaction::{ActionContext, Kind};
 
 pub struct Router {
     context: Context,
@@ -293,7 +293,13 @@ impl crate::cloud_provider::service::Router for Router {
 
         match check_result {
             Ok(_) => {}
-            Err(_) => return Err(ServiceError::CheckFailed(Some(ActionContext::new(Kind::Application, self.id().to_string(), self.context().execution_id().to_string())))),
+            Err(_) => {
+                return Err(ServiceError::CheckFailed(Some(ActionContext::new(
+                    Kind::Application,
+                    self.id().to_string(),
+                    self.context().execution_id().to_string(),
+                ))))
+            }
         }
 
         Ok(())
@@ -360,7 +366,11 @@ impl Create for Router {
 
             // check deployment status
             if helm_history_row.is_none() || !helm_history_row.unwrap().is_successfully_deployed() {
-                return Err(ServiceError::OnCreateFailed(Some(ActionContext::new(Kind::Application, self.id().to_string(), self.context().execution_id().to_string()))));
+                return Err(ServiceError::OnCreateFailed(Some(ActionContext::new(
+                    Kind::Application,
+                    self.id().to_string(),
+                    self.context().execution_id().to_string(),
+                ))));
             }
         }
 
@@ -386,14 +396,20 @@ impl Create for Router {
 
         // check deployment status
         if helm_history_row.is_none() || !helm_history_row.unwrap().is_successfully_deployed() {
-            return Err(ServiceError::OnCreateFailed(Some(ActionContext::new(Kind::Application, self.id().to_string(), self.context().execution_id().to_string()))));
+            return Err(ServiceError::OnCreateFailed(Some(ActionContext::new(
+                Kind::Application,
+                self.id().to_string(),
+                self.context().execution_id().to_string(),
+            ))));
         }
 
         Ok(())
     }
 
     fn on_create_check(&self) -> Result<(), ServiceError> {
-        self.check_domains()
+        // Todo: manage it properly to avoid timeouts
+        //self.check_domains()
+        Ok(())
     }
 
     fn on_create_error(&self, target: &DeploymentTarget) -> Result<(), ServiceError> {
