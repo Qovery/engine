3eba43e28162842b00f426530c03c9c46774fe65
Paul Ollivier <pollivier@qovery.com>
2020-09-24 11:00:24 +0200

    Fix terraform data access

--- lib/aws/services/common/providers.j2.tf
+++ lib/aws/services/common/providers.j2.tf
@@ -15,8 +15,8 @@ data aws_eks_cluster eks_cluster {
 provider "helm" {
   version = "~> 1.2"
   kubernetes {
-    host = data.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(data.eks_cluster.certificate_authority.0.data)
+    host = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
     load_config_file = false
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
