eecbc0e70a1d86580648bbbe62255279e5171a6c
Pierre Mavro <deimos@deimos.fr>
2020-09-29 15:10:40 +0200

    fix: bad agent commit id

--- lib/aws/bootstrap/helm-qovery-agent.tf
+++ lib/aws/bootstrap/helm-qovery-agent.tf
@@ -27,7 +27,7 @@ resource "helm_release" "qovery_agent_resources" {
 
   set {
     name = "image.tag"
-    value = "f82326cdddaa5758249bd46e81343fc99dc5782"
+    value = "cf82326cdddaa5758249bd46e81343fc99dc5782"
   }
 
   set {
