be98bb21c49026c63e8e5cf2e629c954d2b8cfab
Pierre Mavro <pmavro@qovery.com>
2020-11-03 00:55:26 +0100

    fix: missing branch name to gitlab job

--- helper.sh
+++ helper.sh
@@ -21,10 +21,11 @@ function run_tests() {
   test -z $GITLAB_PROJECT_ID && variable_not_found "GITLAB_PROJECT_ID"
   test -z $GITLAB_TOKEN && variable_not_found "GITLAB_TOKEN"
   test -z $GITLAB_PERSONAL_TOKEN && variable_not_found "GITLAB_PERSONAL_TOKEN"
+  test -z $GITHUB_BRANCH && variable_not_found "GITHUB_BRANCH"
   GITLAB_REF="dev"
 
   echo "Requesting Gitlab pipeline"
-  pipeline_id=$(curl -s -X POST -F "token=$GITLAB_TOKEN" -F "ref=$GITLAB_REF" -F "variables[GITHUB_ENGINE_BRANCH_NAME]=$(echo $GITHUB_CONTEXT | jq --raw-output '.ref')" -F "variables[TESTS_TYPE]=$TESTS_TYPE" https://gitlab.com/api/v4/projects/$GITLAB_PROJECT_ID/trigger/pipeline | jq --raw-output '.id')
+  pipeline_id=$(curl -s -X POST -F "token=$GITLAB_TOKEN" -F "ref=$GITLAB_REF" -F "variables[GITHUB_ENGINE_BRANCH_NAME]=$GITHUB_BRANCH" -F "variables[TESTS_TYPE]=$TESTS_TYPE" https://gitlab.com/api/v4/projects/$GITLAB_PROJECT_ID/trigger/pipeline | jq --raw-output '.id')
   if [ $(echo $pipeline_id | egrep -c '^[0-9]+$') -eq 0 ] ; then
     echo "Pipeline ID is not correct, we expected a number and got: $pipeline_id"
     exit 1
