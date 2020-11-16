f9cb73d5366362ebbf29a742139040aa5631ff94
marc <marc.anthony.richard@gmail.com>
2020-11-16 13:16:08 +0100

    retry init 5 times, and apply before destroy on terraform

--- src/cmd/terraform.rs
+++ src/cmd/terraform.rs
@@ -13,21 +13,29 @@ use serde_json::Value;
 
 use crate::cmd::utilities::exec_with_envs_and_output;
 use crate::constants::{KUBECONFIG, TF_PLUGIN_CACHE_DIR};
-use crate::error::SimpleError;
+use crate::error::{SimpleError, SimpleErrorKind};
 
-fn terraform_exec_with_init_validate(
-    root_dir: &str,
-    first_time_init_terraform: bool,
-) -> Result<(), SimpleError> {
+fn terraform_exec_with_init_validate(root_dir: &str) -> Result<(), SimpleError> {
     // terraform init
-    let init_args = if first_time_init_terraform {
-        vec!["init"]
-    } else {
-        vec!["init"]
-    };
+    let result = retry::retry(Fibonacci::from_millis(3000).take(5), || {
+        let try_result = terraform_exec(root_dir, vec!["init"]);
+        match try_result {
+            Ok(out) => OperationResult::Ok(out),
+            Err(err) => OperationResult::Err(format!("Command error: {:?}", err)),
+        }
+    });
 
-    //TODO print
-    terraform_exec(root_dir, init_args)?;
+    match result {
+        Err(err) => match err {
+            retry::Error::Operation {
+                error: _,
+                total_delay: _,
+                tries: _,
+            } => Ok(Some(false)),
+            retry::Error::Internal(err) => Err(SimpleError::new(SimpleErrorKind::Other, Some(err))),
+        },
+        Ok(_) => Ok(Some(true)),
+    };
 
     // terraform validate config
     terraform_exec(root_dir, vec!["validate"])?;
@@ -74,9 +82,10 @@ pub fn terraform_exec_with_init_validate_plan_apply(
     Ok(())
 }
 
-pub fn terraform_exec_with_init_validate_destroy(root_dir: &str) -> Result<(), SimpleError> {
+pub fn terraform_exec_with_init_plan_apply_destroy(root_dir: &str) -> Result<(), SimpleError> {
     // terraform init and plan
-    terraform_exec_with_init_validate(root_dir, false);
+    // should apply before destroy to be sure destroy will compute on all ressources
+    terraform_exec_with_init_validate_plan_apply(root_dir, false, false);
 
     // terraform destroy
     terraform_exec(root_dir, vec!["destroy", "-auto-approve"])
