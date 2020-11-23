99338cfbd3ec349a03655f27ecadf666af791674
Pierre Mavro <deimos@deimos.fr>
2020-11-23 16:01:51 +0100

    feat: adding new tests

--- helper.sh
+++ helper.sh
@@ -73,7 +73,7 @@ function get_gitlab_engine_commit_id() {
 
 function get_github_engine_commit_id() {
   # Ensure we're in the correct folder
-  if [ $(git config --get remote.origin.url | $grep -c "github.com:Qovery/engine.git") -ne 1 ] ; then
+  if [ $(git config --get remote.origin.url | $grep "Qovery/engine.git" | $grep -c github) -ne 1 ] ; then
     (fatal "You're not in the correct directory and should be in the github repo: $(pwd)")
   fi
   git rev-parse HEAD
