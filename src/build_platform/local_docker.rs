16a865909738a93fc480c013a14ebfc57db3e0c3
Romaric Philogene <evoxmusic@gmail.com>
2020-11-28 22:59:17 +0100

    chore: local_docker.rs build cache is turned on by default but it can be overridden by the user with a special env variable `QOVERY_DISABLE_BUILD_CACHE`.

--- src/build_platform/local_docker.rs
+++ src/build_platform/local_docker.rs
@@ -178,22 +178,35 @@ impl BuildPlatform for LocalDocker {
             _ => {}
         }
 
-        let env_var_args = &build
-            .options
-            .environment_variables
-            .iter()
-            .map(|ev| format!("{}={}", ev.key, ev.value))
-            .collect::<Vec<_>>();
+        let mut disable_build_cache = false;
+
+        let mut env_var_args: Vec<String> =
+            Vec::with_capacity(build.options.environment_variables.len());
+
+        for ev in &build.options.environment_variables {
+            if ev.key == "QOVERY_DISABLE_BUILD_CACHE" && ev.value.to_lowercase() == "true" {
+                // this is a special flag to disable build cache dynamically
+                // -- do not pass this env var key/value to as build parameter
+                disable_build_cache = true;
+            } else {
+                env_var_args.push(format!("{}={}", ev.key, ev.value));
+            }
+        }
+
+        let mut docker_args = if disable_build_cache {
+            vec!["build"]
+        } else {
+            vec!["build", "--no-cache"]
+        };
 
         let name_with_tag = build.image.name_with_tag();
-        let mut docker_args = vec![
-            "build",
-            "--no-cache",
+
+        docker_args.extend(vec![
             "-f",
             dockerfile_complete_path.as_str(),
             "-t",
             name_with_tag.as_str(),
-        ];
+        ]);
 
         let mut docker_args = if env_var_args.is_empty() {
             docker_args
