9bd225346d3f8d77a2cb66f05e530c80cb747a2e
marc <marc.anthony.richard@gmail.com>
2020-12-11 11:29:27 +0100

    add tags on clusters to apply new firewall

--- lib/digitalocean/bootstrap/oks-worker-node.j2.tf
+++ lib/digitalocean/bootstrap/oks-worker-node.j2.tf
@@ -4,7 +4,7 @@ resource "digitalocean_kubernetes_node_pool" "app_node_pool_{{ loop.index }}" {
 
   name = "qovery-{{oks_cluster_id}}-{{ loop.index }}"
   size = "{{ oks_worker_node.instance_type }}"
-
+  tags = [digitalocean_tag.cluster_tag.id]
   auto_scale = false
   min_nodes  = "{{ oks_worker_node.min_size }}"
   max_nodes  = "{{ oks_worker_node.max_size }}"
