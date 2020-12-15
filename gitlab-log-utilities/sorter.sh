c1b29323b69cc17ea5f007bfb95ae88126a3692f
marc <marc.anthony.richard@gmail.com>
2020-12-15 17:31:58 +0100

    add some comments

--- gitlab-log-utilities/sorter.sh
+++ gitlab-log-utilities/sorter.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 set -o pipefail
+
+OUTPUT_DIR_TESTS_FILES="gitlab-log-utilities/output"
+JUNIT_REPORT="$OUTPUT_DIR_TESTS_FILES/junit-report.json"
+
+mkdir -p $OUTPUT_DIR_TESTS_FILES
+
 if [ -p /dev/stdin ]; then
   while IFS= read line; do
     # json line ?
     if jq -e . >/dev/null 2>&1 <<<"$line"; then
-      # this is a json file
-      # is junit report or log
+      # this is a json line
       if [ "$(echo "$line" | jq 'has("type")')" == "true" ]; then
-        echo $line >> gitlab-log-utilities/output/junit-report.json
-
+        # it's junit report file
+        echo $line >> "$JUNIT_REPORT"
+        echo $line
       elif [ "$(echo "$line" | jq 'has("spans")')" == "true" ]; then
+        # it's a test log line
         filename=$( echo $line | jq -r '.spans[].name' )
-        echo "$line" >> "gitlab-log-utilities/output/$filename"
+        echo "$line" >> "$OUTPUT_DIR_TESTS_FILES/$filename"
       fi
     elif
+      # test are not in json format ? print them all anyway
       echo $line
     fi
   done
