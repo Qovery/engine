a7be654c2f91174a160c4232323c8a7e16d771db
marc <mrichard@qovery.com>
2020-12-28 09:53:41 +0100

    debug

--- gitlab-log-utilities/failed_test_printer.sh
+++ gitlab-log-utilities/failed_test_printer.sh
@@ -3,8 +3,13 @@
 OUTPUT_DIR_TESTS_FILES="/builds/qovery/qovery-engine/gitlab-log-utilities/output"
 JUNIT_REPORT="$OUTPUT_DIR_TESTS_FILES/junit-report.json"
 
+echo "===================================================="
+echo "            DEBUG"
+echo "===================================================="
 
 cd $OUTPUT_DIR_TESTS_FILES
+pwd
+ls
 
 echo "===================================================="
 echo "            GENERATED OUTPUT TESTS"
