b3f2bdc2cae20df4527bb9ea7612771a0800e284
Romain GERARD <erebe@erebe.eu>
2021-01-26 15:22:55 +0100

    Linting: better message

--- helper.sh
+++ helper.sh
@@ -347,8 +347,17 @@ function lint() {
   
   set -e
 
+  echo "###################################################"
+  echo "                   CARGO FMT                       "
+  echo "###################################################"
   cargo fmt --all -- --check --color=always  || (echo "Use cargo fmt to format your code"; exit 1)
-  RUSTFLAGS="--deny warnings" cargo build
+
+
+  echo "###################################################"
+  echo "             CARGO BUILD NO WARNING                "
+  echo "###################################################"
+  RUSTFLAGS="--deny warnings" cargo build || (echo "Solve your warnings to succeed"; exit 1)
+
   # FIXME fix warning in the engine and enable clippy
   # cargo clippy
 }
