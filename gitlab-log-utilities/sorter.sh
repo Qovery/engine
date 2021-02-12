20441bd6f860ea9a758f6a21d18e08cf0eefb18b
Pierre Mavro <deimos@deimos.fr>
2021-02-12 18:23:57 +0100

    feat: drastically speed up log sorter 30min -> -3s

--- gitlab-log-utilities/sorter.sh
+++ gitlab-log-utilities/sorter.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-set -o pipefail
+set -e
 
-OUTPUT_DIR_TESTS_FILES="/builds/qovery/qovery-engine/gitlab-log-utilities/output"
+OUTPUT_DIR_TESTS_FILES="/home/pmavro/Downloads/tmp/gitlab-log-utilities/output"
 JUNIT_REPORT="$OUTPUT_DIR_TESTS_FILES/junit-report.json"
+OUTPUT_FILE=$1
 
-touch JUNIT_REPORT
-while IFS= read -r line; do
-if jq -e . >/dev/null 2>&1 <<<"$line"; then
-    # this is a json line
-    if [ $(echo "$line" | grep -c '"type":') -eq 1 ]; then
-      # it's junit report file
-      echo $line >> "$JUNIT_REPORT"
-    elif [ $(echo "$line" | grep -c '"spans":') -eq 1 ]; then
-        # it's a test log line
-        filename=$( echo $line | sed -r 's/^(.+),"name":"test"(.+)$/\1\2/g' | jq -r '.spans[].name' )
-        echo "$line" >> "$OUTPUT_DIR_TESTS_FILES/$filename"
-    fi
-fi
-done < $1
+echo "[+] Sorting logs output to files:"
+
+# generate junit file
+grep '^{ "type": "' $OUTPUT_FILE > $JUNIT_REPORT
+# generate logs files
+for test_name in $(grep '"spans":' $OUTPUT_FILE | sed -r 's/^(.+),"name":"test"(.+)$/\1\2/g' | jq -r '.spans[].name' | sort | uniq) ; do
+    echo "-> processing $test_name"
+    grep $test_name $OUTPUT_FILE | grep '^{"timestamp"' > $OUTPUT_DIR_TESTS_FILES/$test_name
+done
