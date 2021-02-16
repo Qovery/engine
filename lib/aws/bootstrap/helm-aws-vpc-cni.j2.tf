07d5a22f03f785de6b28ce412167ff21e396e4a6
Pierre Mavro <pmavro@qovery.com>
2021-02-16 11:02:10 +0100

    feat: add vault support to store sensitive info

--- lib/aws/bootstrap/helm-aws-vpc-cni.j2.tf
+++ lib/aws/bootstrap/helm-aws-vpc-cni.j2.tf
@@ -90,6 +90,7 @@ resource "helm_release" "aws_vpc_cni" {
 
   depends_on = [
     aws_eks_cluster.eks_cluster,
+    vault_generic_secret.cluster-access,
     null_resource.delete_aws_managed_cni,
   ]
 }
