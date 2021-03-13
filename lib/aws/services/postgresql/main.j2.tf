4d79d21118d2a133802f945db0985ae3df34064a
Pierre Mavro <pmavro@qovery.com>
2021-03-13 11:07:45 +0100

    feat: keep last backup once a managed db is deleted

--- lib/aws/services/postgresql/main.j2.tf
+++ lib/aws/services/postgresql/main.j2.tf
@@ -115,7 +115,7 @@ resource "aws_db_instance" "postgresql_instance" {
   # Backups
   backup_retention_period = var.backup_retention_period
   backup_window = var.backup_window
-  skip_final_snapshot = true
+  skip_final_snapshot = var.delete_automated_backups
   delete_automated_backups = var.delete_automated_backups
 
 }
