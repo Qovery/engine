cc5d7c27497ebaa7ce2e8a3e944976e1c024f308
Romain GERARD <erebe@erebe.eu>
2021-05-05 17:39:10 +0200

    Use a single instance of Tokio runtime

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,9 +1,20 @@
+use lazy_static::lazy_static;
 use std::future::Future;
+use std::sync::Mutex;
+use tokio::runtime::{Builder, Runtime};
 
-use tokio::runtime::Runtime;
+lazy_static! {
+    static ref TOKIO_RUNTIME: Mutex<Runtime> = Mutex::new({
+        Builder::new()
+            .basic_scheduler()
+            .thread_name("tokio-engine-blocking")
+            .max_threads(1)
+            .enable_all()
+            .build()
+            .unwrap()
+    });
+}
 
-pub fn async_run<F: Future>(future: F) -> F::Output {
-    // TODO improve - is it efficient to create a Runtime at each exec?
-    let mut runtime = Runtime::new().expect("unable to create a tokio runtime");
-    runtime.block_on(future)
+pub fn block_on<F: Future>(future: F) -> F::Output {
+    TOKIO_RUNTIME.lock().unwrap().block_on(future)
 }
