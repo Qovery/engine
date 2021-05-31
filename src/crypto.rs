cb24127ce34a31b0927e6cd5202b640d929fc56a
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2021-05-31 16:05:54 +0200

    tests: crypto add basics unit tests

--- src/crypto.rs
+++ src/crypto.rs
@@ -12,3 +12,74 @@ pub fn to_sha1_truncate_16(input: &str) -> String {
     hash_str.truncate(16);
     hash_str
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestCase<'a> {
+        input: &'a str,
+        expected_output: String,
+        description: &'a str,
+    }
+
+    #[test]
+    fn test_to_sha1() {
+        // setup:
+        let test_cases: Vec<TestCase> = vec![
+            TestCase {
+                input: "",
+                expected_output: String::from("da39a3ee5e6b4b0d3255bfef95601890afd80709"),
+                description: "empty &str input",
+            },
+            TestCase {
+                input: "abc",
+                expected_output: String::from("a9993e364706816aba3e25717850c26c9cd0d89d"),
+                description: "simple small input 1",
+            },
+            TestCase {
+                input: "abcdefghijklmnopqrstuvwxyz",
+                expected_output: String::from("32d10c7b8cf96570ca04ce37f2a19d84240d3a89"),
+                description: "simple small input 1",
+            },
+        ];
+
+        for tc in test_cases {
+            // execute:
+            let result = to_sha1(tc.input);
+
+            // verify:
+            assert_eq!(tc.expected_output, result, "case {} : '{}'", tc.description, tc.input);
+        }
+    }
+
+    #[test]
+    fn test_to_sha1_truncate_16() {
+        // setup:
+        let test_cases: Vec<TestCase> = vec![
+            TestCase {
+                input: "",
+                expected_output: String::from("da39a3ee5e6b4b0d"),
+                description: "empty &str input",
+            },
+            TestCase {
+                input: "abc",
+                expected_output: String::from("a9993e364706816a"),
+                description: "simple small input 1",
+            },
+            TestCase {
+                input: "abcdefghijklmnopqrstuvwxyz",
+                expected_output: String::from("32d10c7b8cf96570"),
+                description: "simple small input 1",
+            },
+        ];
+
+        for tc in test_cases {
+            // execute:
+            let result = to_sha1_truncate_16(tc.input);
+
+            // verify:
+            assert_eq!(tc.expected_output, result, "case {} : '{}'", tc.description, tc.input);
+        }
+    }
+}
