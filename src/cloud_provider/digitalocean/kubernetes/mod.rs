908c7a7385ab988d31fe45b7f1bc4591d2b804b1
Romain GERARD <erebe@erebe.eu>
2021-06-15 10:18:11 +0200

    fix

--- src/cloud_provider/digitalocean/kubernetes/mod.rs
+++ src/cloud_provider/digitalocean/kubernetes/mod.rs
@@ -221,8 +221,13 @@ impl<'a> DOKS<'a> {
             self.cloud_provider().terraform_state_credentials().region.as_str(),
         );
 
+        context.insert("nginx_enable_horizontal_autoscaler", "true");
+        context.insert("nginx_minimum_replicas", "2");
+        context.insert("nginx_requests_cpu", "250m");
+        context.insert("nginx_requests_memory", "384Mi");
+        context.insert("nginx_limit_cpu", "1");
+        context.insert("nginx_limit_memory", "384Mi");
         context.insert("aws_terraform_backend_dynamodb_table", "qovery-terrafom-tfstates");
-
         context.insert("aws_terraform_backend_bucket", "qovery-terrafom-tfstates");
 
         // kubernetes workers
