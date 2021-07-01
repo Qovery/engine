891ae50489080019871c26014112809f0a90239e
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2021-07-01 18:21:41 +0200

    feat: terraform add object storage versioning support

--- lib/scaleway/bootstrap/ks-object-bucket.tf
+++ lib/scaleway/bootstrap/ks-object-bucket.tf
@@ -3,5 +3,9 @@ resource "scaleway_object_bucket" "object-bucket" {
   # NOTE: name supports only alpha-numerics!
   name = "test-cluster" # TODO(benjaminch): use var.s3_bucket_kubeconfig
   acl  = "private"
+  versioning {
+    enabled = true
+  }
+
   tags = local.tags_ks
 }
