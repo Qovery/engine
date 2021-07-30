a9bd50d40fb6ee5d57daf3501be6b23f5969940b
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2021-07-30 13:46:09 +0200

    fix: rename scaleway cluster test script utilities

--- tests/scaleway/mod.rs
+++ tests/scaleway/mod.rs
@@ -2,4 +2,4 @@ mod scw_container_registry;
 mod scw_environment;
 mod scw_kubernetes;
 mod scw_object_storage;
-mod scw_utility_test_kubernetes_kapsule_cluster;
+mod scw_utility_kubernetes_kapsule_test_cluster;
