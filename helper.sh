7d39d6f11505d1424319394c8ab03ce6459ee6aa
Romain GERARD <erebe@erebe.eu>
2021-08-09 10:36:16 +0200

    Fix helper.sh release_to_prod

--- helper.sh
+++ helper.sh
@@ -301,28 +301,28 @@ function get_release_ga() { ## Get globally available release version
 function release_to_prod() { ## Release GA to prod
   prepare_engine
   tag=$(generate_image_tag)
-  AWS_ACCESS_KEY_ID=$AWS_PROD_DEPLOY_ACCESS_KEY \
-  AWS_SECRET_ACCESS_KEY=$AWS_PROD_DEPLOY_SECRET_KEY \
+  AWS_ACCESS_KEY_ID="$AWS_PROD_DEPLOY_ACCESS_KEY" \
+  AWS_SECRET_ACCESS_KEY="$AWS_PROD_DEPLOY_SECRET_KEY" \
   AWS_DEFAULT_REGION=eu-west-3 \
-  helm upgrade --kubeconfig $AWS_PROD_KUBECONFIG --install --history-max 50 --wait --namespace qovery qovery-engine \
-  $ENGINE_DIR/lib/common/bootstrap/charts/qovery-engine --set \
-  image.tag="$tag",\
-  environmentVariables.QOVERY_NATS_URL="tls://nats-external.qovery.com:4242",\
-  environmentVariables.QOVERY_NATS_USER="$QOVERY_NATS_USER",\
-  environmentVariables.QOVERY_NATS_PASSWORD="$QOVERY_NATS_PASSWORD",\
-  environmentVariables.CLOUD_PROVIDER="aws",\
-  environmentVariables.LIB_ROOT_DIR="/home/qovery/lib",\
-  environmentVariables.DOCKER_HOST="tcp://0.0.0.0:2375",\
-  environmentVariables.RUST_LOG="DEBUG,rusoto_core::request=info,hyper=info",\
-  environmentVariables.WORKSPACE_ROOT_DIR="/home/qovery",\
-  environmentVariables.VAULT_ADDR="https://vaultemort.qovery.com",\
-  environmentVariables.VAULT_ROLE_ID="$VAULT_ENGINE_PROD_ROLE_ID",\
-  environmentVariables.VAULT_SECRET_ID="$VAULT_ENGINE_PROD_SECRET_ID",\
-  environmentVariables.WORKSPACE_ROOT_DIR="/home/qovery",\
-  resources.limits.cpu="1",\
-  resources.limits.memory="2Gi",\
-  resources.requests.cpu="500m",\
-  resources.requests.memory="2Gi"
+  helm upgrade --kubeconfig="$AWS_PROD_KUBECONFIG" --install --history-max 50 --wait --namespace qovery qovery-engine \
+  $ENGINE_DIR/lib/common/bootstrap/charts/qovery-engine \
+  --set image.tag="$tag",\
+environmentVariables.QOVERY_NATS_URL="tls://nats-external.qovery.com:4242",\
+environmentVariables.QOVERY_NATS_USER="$QOVERY_NATS_USER",\
+environmentVariables.QOVERY_NATS_PASSWORD="$QOVERY_NATS_PASSWORD",\
+environmentVariables.CLOUD_PROVIDER="aws",\
+environmentVariables.LIB_ROOT_DIR="/home/qovery/lib",\
+environmentVariables.DOCKER_HOST="tcp://0.0.0.0:2375",\
+environmentVariables.RUST_LOG="DEBUG,rusoto_core::request=info,hyper=info",\
+environmentVariables.WORKSPACE_ROOT_DIR="/home/qovery",\
+environmentVariables.VAULT_ADDR="https://vaultemort.qovery.com",\
+environmentVariables.VAULT_ROLE_ID="$VAULT_ENGINE_PROD_ROLE_ID",\
+environmentVariables.VAULT_SECRET_ID="$VAULT_ENGINE_PROD_SECRET_ID",\
+environmentVariables.WORKSPACE_ROOT_DIR="/home/qovery",\
+resources.limits.cpu="1",\
+resources.limits.memory="2Gi",\
+resources.requests.cpu="500m",\
+resources.requests.memory="2Gi"
 }
 
 function upgrade_on_dev() {
