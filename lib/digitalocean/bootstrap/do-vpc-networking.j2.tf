bddd3d74cd46bca12a77dd30f48235a05e841629
Benjamin <benjamin.chastanier@gmail.com>
2021-08-25 12:23:54 +0200

    fix: DO cidr set var injection (#341)

--- lib/digitalocean/bootstrap/do-vpc-networking.j2.tf
+++ lib/digitalocean/bootstrap/do-vpc-networking.j2.tf
@@ -2,7 +2,7 @@ resource "digitalocean_vpc" "qovery_vpc" {
   name     = var.vpc_name
   region   = var.region
 
-{%- if 'manual' == var.vpc_cidr_set %}
+{%- if 'manual' == do_vpc_cidr_set %}
   # Note: if set to manual, then it means a CIDR has been specified and needs to be declared.
   # The range of IP addresses for the VPC in CIDR notation.
   # Network ranges cannot overlap with other networks in the same account and must be in range of private addresses as defined in RFC1918.
