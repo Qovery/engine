f23f7d8b573afcda960a25e7f934f24bf99a6230
Pierre Mavro <pmavro@qovery.com>
2021-09-06 11:19:27 +0200

    feat: add missing tests for scaleway

--- tests/scaleway/scw_databases.rs
+++ tests/scaleway/scw_databases.rs
@@ -6,10 +6,10 @@ use qovery_engine::models::{
     Kind,
 };
 use qovery_engine::transaction::TransactionResult;
-use test_utilities::scaleway::working_minimal_environment;
 use test_utilities::utilities::{context, engine_run_test, generate_id, init, FuncTestsSecrets};
 
 use crate::scaleway::scw_environment::{delete_environment, deploy_environment};
+use test_utilities::scaleway::working_minimal_environment;
 
 /**
  **
@@ -118,69 +118,37 @@ fn test_postgresql_configuration(
 }
 
 // Postgres environment environment
-#[cfg(feature = "test-aws-self-hosted")]
+#[cfg(feature = "test-scw-self-hosted")]
 #[named]
 #[test]
 fn postgresql_v10_deploy_a_working_dev_environment() {
     let context = context();
     let secrets = FuncTestsSecrets::new();
-    let environment = test_utilities::aws::working_minimal_environment(&context, secrets.clone());
+    let environment = working_minimal_environment(&context, secrets.clone());
     test_postgresql_configuration(context, environment, secrets, "10", function_name!());
 }
 
-#[cfg(feature = "test-aws-self-hosted")]
+#[cfg(feature = "test-scw-self-hosted")]
 #[named]
 #[test]
 fn postgresql_v11_deploy_a_working_dev_environment() {
     let context = context();
     let secrets = FuncTestsSecrets::new();
-    let environment = test_utilities::aws::working_minimal_environment(&context, secrets.clone());
+    let environment = working_minimal_environment(&context, secrets.clone());
     test_postgresql_configuration(context, environment, secrets, "11", function_name!());
 }
 
-#[cfg(feature = "test-aws-self-hosted")]
+#[cfg(feature = "test-scw-self-hosted")]
 #[named]
 #[test]
 fn postgresql_v12_deploy_a_working_dev_environment() {
     let context = context();
     let secrets = FuncTestsSecrets::new();
-    let environment = test_utilities::aws::working_minimal_environment(&context, secrets.clone());
+    let environment = working_minimal_environment(&context, secrets.clone());
     test_postgresql_configuration(context, environment, secrets, "12", function_name!());
 }
 
 // Postgres production environment
-#[cfg(feature = "test-aws-managed-services")]
-#[named]
-#[test]
-fn postgresql_v10_deploy_a_working_prod_environment() {
-    let context = context();
-    let secrets = FuncTestsSecrets::new();
-    let mut environment = test_utilities::aws::working_minimal_environment(&context, secrets.clone());
-    environment.kind = Kind::Production;
-    test_postgresql_configuration(context, environment, secrets, "10", function_name!());
-}
-
-#[cfg(feature = "test-aws-managed-services")]
-#[named]
-#[test]
-fn postgresql_v11_deploy_a_working_prod_environment() {
-    let context = context();
-    let secrets = FuncTestsSecrets::new();
-    let mut environment = test_utilities::aws::working_minimal_environment(&context, secrets.clone());
-    environment.kind = Kind::Production;
-    test_postgresql_configuration(context, environment, secrets, "11", function_name!());
-}
-
-#[cfg(feature = "test-aws-managed-services")]
-#[named]
-#[test]
-fn postgresql_v12_deploy_a_working_prod_environment() {
-    let context = context();
-    let secrets = FuncTestsSecrets::new();
-    let mut environment = test_utilities::aws::working_minimal_environment(&context, secrets.clone());
-    environment.kind = Kind::Production;
-    test_postgresql_configuration(context, environment, secrets, "12", function_name!());
-}
 
 /**
  **
@@ -333,4 +301,282 @@ fn mongodb_v4_4_deploy_a_working_dev_environment() {
     let secrets = FuncTestsSecrets::new();
     let environment = working_minimal_environment(&context, secrets.clone());
     test_mongodb_configuration(context, environment, secrets, "4.4", function_name!());
+}
+
+/**
+ **
+ ** MySQL tests
+ **
+ **/
+
+fn test_mysql_configuration(
+    context: Context,
+    mut environment: Environment,
+    secrets: FuncTestsSecrets,
+    version: &str,
+    test_name: &str,
+) {
+    engine_run_test(|| {
+        init();
+
+        let span = span!(Level::INFO, "test", name = test_name);
+        let _enter = span.enter();
+
+        let deletion_context = context.clone_not_same_execution_id();
+
+        let app_name = format!("mysql-app-{}", generate_id());
+        let database_host = format!("mysql-{}.{}", generate_id(), secrets.DEFAULT_TEST_DOMAIN.unwrap());
+
+        let database_port = 3306;
+        let database_db_name = "mysqldatabase".to_string();
+        let database_username = "superuser".to_string();
+        let database_password = generate_id();
+
+        let _is_rds = match environment.kind {
+            Kind::Production => true,
+            Kind::Development => false,
+        };
+
+        environment.databases = vec![Database {
+            kind: DatabaseKind::Mysql,
+            action: Action::Create,
+            id: generate_id(),
+            name: database_db_name.clone(),
+            version: version.to_string(),
+            fqdn_id: "mysql-".to_string() + generate_id().as_str(),
+            fqdn: database_host.clone(),
+            port: database_port.clone(),
+            username: database_username.clone(),
+            password: database_password.clone(),
+            total_cpus: "500m".to_string(),
+            total_ram_in_mib: 512,
+            disk_size_in_gib: 10,
+            database_instance_type: "not-used".to_string(),
+            database_disk_type: "scw-sbv-ssd-0".to_string(),
+        }];
+        environment.applications = environment
+            .applications
+            .into_iter()
+            .map(|mut app| {
+                app.branch = app_name.clone();
+                app.commit_id = "fc8a87b39cdee84bb789893fb823e3e62a1999c0".to_string();
+                app.private_port = Some(1234);
+                app.dockerfile_path = Some(format!("Dockerfile-{}", version));
+                app.environment_variables = vec![
+                    // EnvironmentVariable {
+                    //     key: "ENABLE_DEBUG".to_string(),
+                    //     value: "true".to_string(),
+                    // },
+                    // EnvironmentVariable {
+                    //     key: "DEBUG_PAUSE".to_string(),
+                    //     value: "true".to_string(),
+                    // },
+                    EnvironmentVariable {
+                        key: "MYSQL_HOST".to_string(),
+                        value: database_host.clone(),
+                    },
+                    EnvironmentVariable {
+                        key: "MYSQL_PORT".to_string(),
+                        value: database_port.clone().to_string(),
+                    },
+                    EnvironmentVariable {
+                        key: "MYSQL_DBNAME".to_string(),
+                        value: database_db_name.clone(),
+                    },
+                    EnvironmentVariable {
+                        key: "MYSQL_USERNAME".to_string(),
+                        value: database_username.clone(),
+                    },
+                    EnvironmentVariable {
+                        key: "MYSQL_PASSWORD".to_string(),
+                        value: database_password.clone(),
+                    },
+                ];
+                app
+            })
+            .collect::<Vec<qovery_engine::models::Application>>();
+        environment.routers[0].routes[0].application_name = app_name.clone();
+
+        let mut environment_delete = environment.clone();
+        environment_delete.action = Action::Delete;
+        let ea = EnvironmentAction::Environment(environment);
+        let ea_delete = EnvironmentAction::Environment(environment_delete);
+
+        match deploy_environment(&context, ea) {
+            TransactionResult::Ok => assert!(true),
+            TransactionResult::Rollback(_) => assert!(false),
+            TransactionResult::UnrecoverableError(_, _) => assert!(false),
+        };
+
+        // todo: check the database disk is here and with correct size
+
+        match delete_environment(&deletion_context, ea_delete) {
+            TransactionResult::Ok => assert!(true),
+            TransactionResult::Rollback(_) => assert!(false),
+            TransactionResult::UnrecoverableError(_, _) => assert!(false),
+        };
+
+        return test_name.to_string();
+    })
+}
+
+// MySQL self-hosted environment
+#[cfg(feature = "test-scw-self-hosted")]
+#[named]
+#[test]
+fn mysql_v5_7_deploy_a_working_dev_environment() {
+    let context = context();
+    let secrets = FuncTestsSecrets::new();
+    let environment = working_minimal_environment(&context, secrets.clone());
+    test_mysql_configuration(context, environment, secrets, "5.7", function_name!());
+}
+
+#[cfg(feature = "test-scw-self-hosted")]
+#[named]
+#[test]
+fn mysql_v8_deploy_a_working_dev_environment() {
+    let context = context();
+    let secrets = FuncTestsSecrets::new();
+    let environment = working_minimal_environment(&context, secrets.clone());
+    test_mysql_configuration(context, environment, secrets, "8.0", function_name!());
+}
+
+// MySQL production environment
+
+/**
+ **
+ ** Redis tests
+ **
+ **/
+
+fn test_redis_configuration(
+    context: Context,
+    mut environment: Environment,
+    secrets: FuncTestsSecrets,
+    version: &str,
+    test_name: &str,
+) {
+    engine_run_test(|| {
+        init();
+
+        let span = span!(Level::INFO, "test", name = test_name);
+        let _enter = span.enter();
+
+        let context_for_delete = context.clone_not_same_execution_id();
+
+        let app_name = format!("redis-app-{}", generate_id());
+        let database_host = format!("redis-{}.{}", generate_id(), secrets.DEFAULT_TEST_DOMAIN.unwrap());
+        let database_port = 6379;
+        let database_db_name = "my-redis".to_string();
+        let database_username = "superuser".to_string();
+        let database_password = generate_id();
+
+        let is_elasticache = match environment.kind {
+            Kind::Production => true,
+            Kind::Development => false,
+        };
+
+        environment.databases = vec![Database {
+            kind: DatabaseKind::Redis,
+            action: Action::Create,
+            id: generate_id(),
+            name: database_db_name.clone(),
+            version: version.to_string(),
+            fqdn_id: "redis-".to_string() + generate_id().as_str(),
+            fqdn: database_host.clone(),
+            port: database_port.clone(),
+            username: database_username.clone(),
+            password: database_password.clone(),
+            total_cpus: "500m".to_string(),
+            total_ram_in_mib: 512,
+            disk_size_in_gib: 10,
+            database_instance_type: "not-used".to_string(),
+            database_disk_type: "scw-sbv-ssd-0".to_string(),
+        }];
+        environment.applications = environment
+            .applications
+            .into_iter()
+            .map(|mut app| {
+                app.name = app_name.clone();
+                app.branch = "redis-app".to_string();
+                app.commit_id = "80ad41fbe9549f8de8dbe2ca4dd5d23e8ffc92de".to_string();
+                app.private_port = Some(1234);
+                app.dockerfile_path = Some(format!("Dockerfile-{}", version));
+                app.environment_variables = vec![
+                    // EnvironmentVariable {
+                    //     key: "ENABLE_DEBUG".to_string(),
+                    //     value: "true".to_string(),
+                    // },
+                    // EnvironmentVariable {
+                    //     key: "DEBUG_PAUSE".to_string(),
+                    //     value: "true".to_string(),
+                    // },
+                    EnvironmentVariable {
+                        key: "IS_ELASTICCACHE".to_string(),
+                        value: is_elasticache.to_string(),
+                    },
+                    EnvironmentVariable {
+                        key: "REDIS_HOST".to_string(),
+                        value: database_host.clone(),
+                    },
+                    EnvironmentVariable {
+                        key: "REDIS_PORT".to_string(),
+                        value: database_port.clone().to_string(),
+                    },
+                    EnvironmentVariable {
+                        key: "REDIS_USERNAME".to_string(),
+                        value: database_username.clone(),
+                    },
+                    EnvironmentVariable {
+                        key: "REDIS_PASSWORD".to_string(),
+                        value: database_password.clone(),
+                    },
+                ];
+                app
+            })
+            .collect::<Vec<qovery_engine::models::Application>>();
+        environment.routers[0].routes[0].application_name = app_name.clone();
+
+        let mut environment_delete = environment.clone();
+        environment_delete.action = Action::Delete;
+        let ea = EnvironmentAction::Environment(environment);
+        let ea_delete = EnvironmentAction::Environment(environment_delete);
+
+        match deploy_environment(&context, ea) {
+            TransactionResult::Ok => assert!(true),
+            TransactionResult::Rollback(_) => assert!(false),
+            TransactionResult::UnrecoverableError(_, _) => assert!(false),
+        };
+
+        // todo: check the database disk is here and with correct size
+
+        match delete_environment(&context_for_delete, ea_delete) {
+            TransactionResult::Ok => assert!(true),
+            TransactionResult::Rollback(_) => assert!(false),
+            TransactionResult::UnrecoverableError(_, _) => assert!(true),
+        };
+
+        return test_name.to_string();
+    })
+}
+
+// Redis self-hosted environment
+#[cfg(feature = "test-scw-self-hosted")]
+#[named]
+#[test]
+fn redis_v5_deploy_a_working_dev_environment() {
+    let context = context();
+    let secrets = FuncTestsSecrets::new();
+    let environment = working_minimal_environment(&context, secrets.clone());
+    test_redis_configuration(context, environment, secrets, "5", function_name!());
+}
+
+#[cfg(feature = "test-scw-self-hosted")]
+#[named]
+#[test]
+fn redis_v6_deploy_a_working_dev_environment() {
+    let context = context();
+    let secrets = FuncTestsSecrets::new();
+    let environment = working_minimal_environment(&context, secrets.clone());
+    test_redis_configuration(context, environment, secrets, "6", function_name!());
 }
