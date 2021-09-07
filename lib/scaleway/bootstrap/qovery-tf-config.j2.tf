a8b71e6f452991fa25c214c6418af926ef25fabc
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2021-09-07 18:26:12 +0200

    fix: fix loki_storage_config_scaleway_s3 url

--- lib/scaleway/bootstrap/qovery-tf-config.j2.tf
+++ lib/scaleway/bootstrap/qovery-tf-config.j2.tf
@@ -1,7 +1,7 @@
 locals {
   qovery_tf_config = <<TF_CONFIG
 {
-  "loki_storage_config_scaleway_s3": "s3://${urlencode(var.scaleway_access_key)}:${urlencode(var.scaleway_secret_key)}@{{ object_storage_logs_bucket }}.s3.${var.region}.scw.cloud"
+  "loki_storage_config_scaleway_s3": "s3://${urlencode(var.scaleway_access_key)}:${urlencode(var.scaleway_secret_key)}@s3.${var.region}.scw.cloud/{{ object_storage_logs_bucket }}"
 }
 TF_CONFIG
 }
