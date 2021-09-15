01b1b11ad9541085f7ea81402448103d51907fab
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2021-09-15 18:02:12 +0200

    fix: scaleway add zone, region and depends on node pool

--- lib/scaleway/bootstrap/ks-workers-nodes.j2.tf
+++ lib/scaleway/bootstrap/ks-workers-nodes.j2.tf
@@ -4,12 +4,19 @@ resource "scaleway_k8s_pool" "kubernetes_cluster_workers_{{ loop.index }}" {
   name          = var.kubernetes_cluster_id
   node_type     = "{{ scw_ks_worker_node.instance_type }}"
 
+  region        = var.region
+  zone          = var.zone
+
   # use Scaleway built-in cluster autoscaler
   autoscaling   = {{ scw_ks_pool_autoscale }}
   autohealing   = true
   size          = "{{ scw_ks_worker_node.min_size }}"
   min_size      = "{{ scw_ks_worker_node.min_size }}"
   max_size      = "{{ scw_ks_worker_node.max_size }}"
+
+  depends_on    = [
+    scaleway_k8s_cluster.kubernetes_cluster,
+  ]
 
   tags          =  local.tags_ks_list
 }
