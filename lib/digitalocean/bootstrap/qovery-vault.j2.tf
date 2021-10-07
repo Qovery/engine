7202645a3fa0ccf739e673a44b9f361978236a25
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2021-10-07 17:00:08 +0200

    fix: DO vault cluster information wrong JSON

--- lib/digitalocean/bootstrap/qovery-vault.j2.tf
+++ lib/digitalocean/bootstrap/qovery-vault.j2.tf
@@ -7,19 +7,19 @@ locals {
 resource "vault_generic_secret" "cluster-access" {
   path = "official-clusters-access/${var.organization_id}-${var.kubernetes_cluster_id}"
   data_json = <<EOT
-  {
-    "cloud_provider": "${var.cloud_provider}",
-    "cluster_name": "${var.kubernetes_cluster_name}",
-    "KUBECONFIG_b64": "${local.kubeconfig_base64}",
-    "organization_id": "${var.organization_id}",
-    "test_cluster": "${var.test_cluster}",
-    "grafana_login": "{{ grafana_admin_user }}",
-    "grafana_password": "{{ grafana_admin_password }}",
-    "DIGITAL_OCEAN_DEFAULT_REGION": "${var.region}",
-    "DIGITAL_OCEAN_SPACES_ACCESS_ID": "${var.space_access_id}",
-    "DIGITAL_OCEAN_SPACES_SECRET_ID": "${var.space_secret_key}",
-    "DIGITAL_OCEAN_TOKEN": "{{ digitalocean_token }}",
-  }
+{
+  "cloud_provider": "${var.cloud_provider}",
+  "cluster_name": "${var.kubernetes_cluster_name}",
+  "KUBECONFIG_b64": "${local.kubeconfig_base64}",
+  "organization_id": "${var.organization_id}",
+  "test_cluster": "${var.test_cluster}",
+  "grafana_login": "{{ grafana_admin_user }}",
+  "grafana_password": "{{ grafana_admin_password }}",
+  "DIGITAL_OCEAN_DEFAULT_REGION": "${var.region}",
+  "DIGITAL_OCEAN_SPACES_ACCESS_ID": "${var.space_access_id}",
+  "DIGITAL_OCEAN_SPACES_SECRET_ID": "${var.space_secret_key}",
+  "DIGITAL_OCEAN_TOKEN": "{{ digitalocean_token }}"
+}
 EOT
 
   depends_on = [
