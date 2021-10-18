b01f2becfebfb160f179617cae1ddf519b09cf99
MacLikorne <e.radnai@hotmail.fr>
2021-10-18 10:20:56 +0200

    fix: remove unsopported postgre v14

--- tests/aws/aws_databases.rs
+++ tests/aws/aws_databases.rs
@@ -581,24 +581,6 @@ fn postgresql_v13_deploy_a_working_dev_environment() {
     test_postgresql_configuration(context, environment, secrets, "13", function_name!(), CONTAINER);
 }
 
-#[cfg(feature = "test-aws-self-hosted")]
-#[named]
-#[test]
-fn postgresql_v14_deploy_a_working_dev_environment() {
-    let context = context();
-    let secrets = FuncTestsSecrets::new();
-    let environment = test_utilities::common::working_minimal_environment(
-        &context,
-        AWS_QOVERY_ORGANIZATION_ID,
-        secrets
-            .DEFAULT_TEST_DOMAIN
-            .as_ref()
-            .expect("DEFAULT_TEST_DOMAIN is not set in secrets")
-            .as_str(),
-    );
-    test_postgresql_configuration(context, environment, secrets, "14", function_name!(), CONTAINER);
-}
-
 // Postgres production environment
 #[cfg(feature = "test-aws-managed-services")]
 #[named]
@@ -677,7 +659,6 @@ fn postgresql_v13_deploy_a_working_prod_environment() {
 ** MongoDB tests
 **
 **/
-
 fn test_mongodb_configuration(
     context: Context,
     mut environment: Environment,
