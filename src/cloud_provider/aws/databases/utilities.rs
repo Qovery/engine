b74c46167b5605b6f3c8180cfb2118c1bd2d44b1
Benjamin <benjamin.chastanier@gmail.com>
2021-10-25 10:21:05 +0200

    tests: scw fix self-hosted tests (#465)

--- src/cloud_provider/aws/databases/utilities.rs
+++ src/cloud_provider/aws/databases/utilities.rs
@@ -3,15 +3,6 @@ use crate::error::StringError;
 use crate::models::DatabaseKind;
 use std::str::FromStr;
 
-pub fn rds_name_sanitizer(max_size: usize, prefix: &str, name: &str) -> String {
-    let max_size = max_size - prefix.len();
-    let mut new_name = format!("{}{}", prefix, name.replace("_", "").replace("-", ""));
-    if new_name.chars().count() > max_size {
-        new_name = new_name[..max_size].to_string();
-    }
-    new_name
-}
-
 pub fn get_parameter_group_from_version(version: &str, database_kind: DatabaseKind) -> Result<String, StringError> {
     let version_number = match VersionsNumber::from_str(version) {
         Ok(v) => {
