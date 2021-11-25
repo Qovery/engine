05d4c5924d7925ebed9fb88a5dc6421e4682806b
Pierre Mavro <pmavro@qovery.com>
2021-11-25 11:06:55 +0100

    fix: wrong was eks timeout content (tf doc is wrong)

--- lib/aws/bootstrap/tf-providers-aws.j2.tf
+++ lib/aws/bootstrap/tf-providers-aws.j2.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version    = "~> 3.59.0"
+      version    = "~> 3.66.0"
     }
     external = {
       source = "hashicorp/external"
