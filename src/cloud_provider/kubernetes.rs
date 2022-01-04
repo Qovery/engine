b442e46cc2e2849053e7326388ee73dbf8908186
enzo <e.radnai@hotmail.fr>
2022-01-04 16:12:26 +0100

    fix: change according to core

--- src/cloud_provider/kubernetes.rs
+++ src/cloud_provider/kubernetes.rs
@@ -1070,7 +1070,7 @@ impl NodeGroups {
         min_nodes: i32,
         max_nodes: i32,
         instance_type: String,
-        disk_size_gb: i32,
+        disk_size_in_gib: i32,
     ) -> Result<Self, SimpleError> {
         if min_nodes > max_nodes {
             return Err(SimpleError {
@@ -1087,7 +1087,7 @@ impl NodeGroups {
             min_nodes,
             max_nodes,
             instance_type,
-            disk_size_gb,
+            disk_size_in_gib,
         })
     }
 }
--- src/cloud_provider/scaleway/kubernetes/node.rs
+++ src/cloud_provider/scaleway/kubernetes/node.rs
@@ -107,7 +107,7 @@ mod tests {
                     min_nodes: 2,
                     max_nodes: 2,
                     instance_type: "dev1-l".to_string(),
-                    disk_size_gb: 20
+                    disk_size_in_gib: 20
                 }
             );
         }
