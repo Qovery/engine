683e573f4cc2d43d28dfceb20c09be4922eb3d5b
enzo <e.radnai@hotmail.fr>
2022-01-07 18:19:56 +0100

    feat(ENG_1088): workers status on upgrade

--- src/cloud_provider/digitalocean/kubernetes/mod.rs
+++ src/cloud_provider/digitalocean/kubernetes/mod.rs
@@ -1296,11 +1296,26 @@ impl<'a> Kubernetes for DOKS<'a> {
             self.context.execution_id(),
             terraform_init_validate_plan_apply(temp_dir.as_str(), self.context.is_dry_run_deploy()),
         ) {
-            Ok(_) => {
-                let message = format!("Kubernetes {} nodes have been successfully upgraded", self.name());
-                info!("{}", &message);
-                self.send_to_customer(&message, &listeners_helper);
-            }
+            Ok(_) => match self.check_workers_on_upgrade(upgrade_doks_version) {
+                Ok(_) => {
+                    let message = format!("Kubernetes {} nodes have been successfully upgraded", self.name());
+                    info!("{}", &message);
+                    self.send_to_customer(&message, &listeners_helper);
+                }
+                Err(e) => {
+                    error!(
+                        "Error while upgrading nodes for cluster {} with id {}.",
+                        self.name(),
+                        self.id()
+                    );
+                    return Err(EngineError {
+                        cause: EngineErrorCause::Internal,
+                        scope: EngineErrorScope::Engine,
+                        execution_id: self.context.execution_id().to_string(),
+                        message: e.message,
+                    });
+                }
+            },
             Err(e) => {
                 error!(
                     "Error while upgrading nodes for cluster {} with id {}.",
