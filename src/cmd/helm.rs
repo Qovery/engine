4e62131a2919ea4f26dc209a88c78d288e0d2967
Romain GERARD <erebe@erebe.eu>
2022-02-17 10:39:10 +0100

    Disable hooks

--- src/cmd/helm.rs
+++ src/cmd/helm.rs
@@ -399,6 +399,7 @@ impl Helm {
             self.kubernetes_config.to_str().unwrap_or_default().to_string(),
             "--create-namespace".to_string(),
             "--install".to_string(),
+            "--no-hooks".to_string(), // If engine is launched remotely, it does not have access to the cluster
             "--timeout".to_string(),
             timeout_string.as_str().to_string(),
             "--history-max".to_string(),
@@ -554,18 +555,6 @@ pub fn to_engine_error(event_details: &EventDetails, error: HelmError) -> Engine
     EngineError::new_helm_error(event_details.clone(), error)
 }
 
-//#[cfg(test)]
-//mod tests_simple {
-//    use crate::cmd::helm::helm_exec_with_output;
-//
-//    #[test]
-//    fn check_version() {
-//        let mut output = String::new();
-//        let _ = helm_exec_with_output(&vec!["version"], &vec![], |line| output.push_str(&line), |_line| {});
-//        assert!(output.contains("Version:\"v3.7.2\""));
-//    }
-//}
-
 #[cfg(feature = "test-with-kube")]
 #[cfg(test)]
 mod tests {
@@ -613,6 +602,13 @@ mod tests {
         fn drop(&mut self) {
             self.cleanup()
         }
+    }
+
+    #[test]
+    fn check_version() {
+        let mut output = String::new();
+        let _ = helm_exec_with_output(&vec!["version"], &vec![], |line| output.push_str(&line), |_line| {});
+        assert!(output.contains("Version:\"v3.7.2\""));
     }
 
     #[test]
