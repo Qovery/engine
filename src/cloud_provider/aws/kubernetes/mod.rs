c758ee38f8d01959e4e3e2f6b97903f53173a9ac
Pierre Mavro <deimos@deimos.fr>
2022-03-16 23:30:39 +0100

    feat: add new tag to cloud providers

--- src/cloud_provider/aws/kubernetes/mod.rs
+++ src/cloud_provider/aws/kubernetes/mod.rs
@@ -528,6 +528,7 @@ impl EKS {
         context.insert("eks_cidr_subnet", &eks_cidr_subnet.clone());
         context.insert("kubernetes_cluster_name", &self.name());
         context.insert("kubernetes_cluster_id", self.id());
+        context.insert("kubernetes_full_cluster_id", self.context.cluster_id());
         context.insert("eks_region_cluster_id", region_cluster_id.as_str());
         context.insert("eks_worker_nodes", &self.nodes_groups);
         context.insert("eks_zone_a_subnet_blocks_private", &eks_zone_a_subnet_blocks_private);
