619a1e8bb8f80953ac708e34a54327116881697a
Erèbe - Romain Gerard <erebe@erebe.eu>
2022-03-24 14:21:01 +0100

    Correctly propagate new image tag (#660)

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,6 +1,7 @@
 use std::collections::hash_map::DefaultHasher;
 use std::collections::BTreeMap;
 use std::hash::{Hash, Hasher};
+use std::path::Path;
 
 use reqwest::header;
 use reqwest::header::{HeaderMap, HeaderValue};
@@ -19,11 +20,11 @@ pub fn calculate_hash<T: Hash>(t: &T) -> u64 {
     s.finish()
 }
 
-pub fn get_image_tag(
-    root_path: &String,
-    dockerfile_path: &Option<String>,
+pub fn compute_image_tag<P: AsRef<Path> + Hash, T: AsRef<Path> + Hash>(
+    root_path: P,
+    dockerfile_path: &Option<T>,
     environment_variables: &BTreeMap<String, String>,
-    commit_id: &String,
+    commit_id: &str,
 ) -> String {
     // Image tag == hash(root_path) + commit_id truncate to 127 char
     // https://github.com/distribution/distribution/blob/6affafd1f030087d88f88841bf66a8abe2bf4d24/reference/regexp.go#L41
@@ -38,8 +39,6 @@ pub fn get_image_tag(
         // only use when a Dockerfile is used to prevent build cache miss every single time
         // we redeploy an app with a env var changed with Buildpacks.
         dockerfile_path.hash(&mut hasher);
-
-        // TODO check if the environment variables are used in the Dockerfile and only Hash the one that are used
         environment_variables.hash(&mut hasher);
     }
 
@@ -51,19 +50,19 @@ pub fn get_image_tag(
 
 #[cfg(test)]
 mod tests_utilities {
-    use crate::utilities::get_image_tag;
+    use crate::utilities::compute_image_tag;
     use std::collections::BTreeMap;
 
     #[test]
     fn test_get_image_tag() {
-        let image_tag = get_image_tag(
+        let image_tag = compute_image_tag(
             &"/".to_string(),
             &Some("Dockerfile".to_string()),
             &BTreeMap::new(),
             &"63d8c437337416a7067d3f358197ac47d003fab9".to_string(),
         );
 
-        let image_tag_2 = get_image_tag(
+        let image_tag_2 = compute_image_tag(
             &"/".to_string(),
             &Some("Dockerfile.qovery".to_string()),
             &BTreeMap::new(),
@@ -72,7 +71,7 @@ mod tests_utilities {
 
         assert_ne!(image_tag, image_tag_2);
 
-        let image_tag_3 = get_image_tag(
+        let image_tag_3 = compute_image_tag(
             &"/xxx".to_string(),
             &Some("Dockerfile.qovery".to_string()),
             &BTreeMap::new(),
@@ -81,7 +80,7 @@ mod tests_utilities {
 
         assert_ne!(image_tag, image_tag_3);
 
-        let image_tag_3_2 = get_image_tag(
+        let image_tag_3_2 = compute_image_tag(
             &"/xxx".to_string(),
             &Some("Dockerfile.qovery".to_string()),
             &BTreeMap::new(),
@@ -90,9 +89,9 @@ mod tests_utilities {
 
         assert_eq!(image_tag_3, image_tag_3_2);
 
-        let image_tag_4 = get_image_tag(
+        let image_tag_4 = compute_image_tag(
             &"/".to_string(),
-            &None,
+            &None as &Option<&str>,
             &BTreeMap::new(),
             &"63d8c437337416a7067d3f358197ac47d003fab9".to_string(),
         );
@@ -100,9 +99,9 @@ mod tests_utilities {
         let mut env_vars_5 = BTreeMap::new();
         env_vars_5.insert("toto".to_string(), "key".to_string());
 
-        let image_tag_5 = get_image_tag(
+        let image_tag_5 = compute_image_tag(
             &"/".to_string(),
-            &None,
+            &None as &Option<&str>,
             &env_vars_5,
             &"63d8c437337416a7067d3f358197ac47d003fab9".to_string(),
         );
