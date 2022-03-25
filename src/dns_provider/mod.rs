69b9d9d50c5f7e3bd1702bedafba26288896a60b
BenjaminCh <benjamin.chastanier@gmail.com>
2022-03-25 16:34:38 +0100

    feat: create DNS providers errors types (#655)

--- src/dns_provider/mod.rs
+++ src/dns_provider/mod.rs
@@ -1,14 +1,14 @@
 use std::net::Ipv4Addr;
 
-use crate::errors::EngineError;
-use crate::events::{EnvironmentStep, EventDetails, Stage, ToTransmitter};
+use crate::dns_provider::errors::DnsProviderError;
 use serde::{Deserialize, Serialize};
 
-use crate::models::{Context, Domain, QoveryIdentifier};
+use crate::models::{Context, Domain};
 
 pub mod cloudflare;
+pub mod errors;
 
-pub trait DnsProvider: ToTransmitter {
+pub trait DnsProvider {
     fn context(&self) -> &Context;
     fn provider_name(&self) -> &str;
     fn kind(&self) -> Kind;
@@ -21,19 +21,7 @@ pub trait DnsProvider: ToTransmitter {
     fn token(&self) -> &str;
     fn domain(&self) -> &Domain;
     fn resolvers(&self) -> Vec<Ipv4Addr>;
-    fn is_valid(&self) -> Result<(), EngineError>;
-    fn get_event_details(&self) -> EventDetails {
-        let context = self.context();
-        EventDetails::new(
-            None,
-            QoveryIdentifier::from(context.organization_id().to_string()),
-            QoveryIdentifier::from(context.cluster_id().to_string()),
-            QoveryIdentifier::from(context.execution_id().to_string()),
-            None,
-            Stage::Environment(EnvironmentStep::Deploy),
-            self.to_transmitter(),
-        )
-    }
+    fn is_valid(&self) -> Result<(), DnsProviderError>;
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
