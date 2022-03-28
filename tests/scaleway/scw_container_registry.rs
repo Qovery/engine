9d7d98c615985fbfc4accf49138dd7d3857e4e29
Romain GERARD <erebe@erebe.eu>
2022-03-28 10:36:46 +0200

    FIX CI

--- tests/scaleway/scw_container_registry.rs
+++ tests/scaleway/scw_container_registry.rs
@@ -3,6 +3,8 @@ extern crate test_utilities;
 use self::test_utilities::utilities::{context, FuncTestsSecrets};
 use qovery_engine::cloud_provider::scaleway::application::ScwZone;
 use qovery_engine::container_registry::scaleway_container_registry::ScalewayCR;
+use qovery_engine::models::NoOpProgressListener;
+use std::sync::Arc;
 use tracing::debug;
 use uuid::Uuid;
 
@@ -44,6 +46,7 @@ fn test_get_registry_namespace() {
             scw_secret_key.as_str(),
             scw_default_project_id.as_str(),
             region,
+            Arc::new(Box::new(NoOpProgressListener {})),
         )
         .unwrap();
 
@@ -90,6 +93,7 @@ fn test_create_registry_namespace() {
             scw_secret_key.as_str(),
             scw_default_project_id.as_str(),
             region,
+            Arc::new(Box::new(NoOpProgressListener {})),
         )
         .unwrap();
 
@@ -133,6 +137,7 @@ fn test_delete_registry_namespace() {
             scw_secret_key.as_str(),
             scw_default_project_id.as_str(),
             region,
+            Arc::new(Box::new(NoOpProgressListener {})),
         )
         .unwrap();
 
@@ -170,6 +175,7 @@ fn test_get_or_create_registry_namespace() {
             scw_secret_key.as_str(),
             scw_default_project_id.as_str(),
             region,
+            Arc::new(Box::new(NoOpProgressListener {})),
         )
         .unwrap();
 
