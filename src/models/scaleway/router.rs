6e7347df5e2f27f47d08381bc95c5f51c5d1e37f
Romain GERARD <erebe@erebe.eu>
2022-03-29 14:44:49 +0200

    Router refacto

--- src/models/scaleway/router.rs
+++ src/models/scaleway/router.rs
@@ -1,5 +1,3 @@
-use crate::cloud_provider::models::{CustomDomainDataTemplate, RouteDataTemplate};
-use crate::cloud_provider::service::{default_tera_context, ServiceType};
 use crate::cloud_provider::DeploymentTarget;
 use crate::errors::EngineError;
 use crate::models::router::Router;
@@ -8,67 +6,6 @@ use tera::Context as TeraContext;
 
 impl ToTeraContext for Router<SCW> {
     fn to_tera_context(&self, target: &DeploymentTarget) -> Result<TeraContext, EngineError> {
-        let kubernetes = target.kubernetes;
-        let environment = target.environment;
-        let mut context = default_tera_context(self, kubernetes, environment);
-
-        let applications = environment
-            .stateless_services()
-            .into_iter()
-            .filter(|x| x.service_type() == ServiceType::Application)
-            .collect::<Vec<_>>();
-
-        let custom_domain_data_templates = self
-            .custom_domains
-            .iter()
-            .map(|cd| {
-                let domain_hash = crate::crypto::to_sha1_truncate_16(cd.domain.as_str());
-                CustomDomainDataTemplate {
-                    domain: cd.domain.clone(),
-                    domain_hash,
-                    target_domain: cd.target_domain.clone(),
-                }
-            })
-            .collect::<Vec<_>>();
-
-        let route_data_templates = self
-            .routes
-            .iter()
-            .filter_map(|r| {
-                match applications
-                    .iter()
-                    .find(|app| app.name() == r.application_name.as_str())
-                {
-                    Some(application) => application.private_port().map(|private_port| RouteDataTemplate {
-                        path: r.path.clone(),
-                        application_name: application.sanitized_name(),
-                        application_port: private_port,
-                    }),
-                    _ => None,
-                }
-            })
-            .collect::<Vec<_>>();
-
-        let router_default_domain_hash = crate::crypto::to_sha1_truncate_16(self.default_domain.as_str());
-        let tls_domain = kubernetes.dns_provider().domain().wildcarded();
-
-        context.insert("router_tls_domain", tls_domain.to_string().as_str());
-        context.insert("router_default_domain", self.default_domain.as_str());
-        context.insert("router_default_domain_hash", router_default_domain_hash.as_str());
-        context.insert("custom_domains", &custom_domain_data_templates);
-        context.insert("routes", &route_data_templates);
-        context.insert("spec_acme_email", "tls@qovery.com"); // TODO CHANGE ME
-        context.insert("metadata_annotations_cert_manager_cluster_issuer", "letsencrypt-qovery");
-
-        let lets_encrypt_url = match self.context.is_test_cluster() {
-            true => "https://acme-staging-v02.api.letsencrypt.org/directory",
-            false => "https://acme-v02.api.letsencrypt.org/directory",
-        };
-        context.insert("spec_acme_server", lets_encrypt_url);
-
-        // Nginx
-        context.insert("sticky_sessions_enabled", &self.sticky_sessions_enabled);
-
-        Ok(context)
+        self.default_tera_context(target)
     }
 }
