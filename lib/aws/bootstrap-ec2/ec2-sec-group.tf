b5d71b2abb7988ea9c6023b3a50afb85b0305bb4
Pierre Mavro <deimos@deimos.fr>
2022-04-22 19:58:06 +0200

    feat: add security rules to aws EC2

--- lib/aws/bootstrap-ec2/ec2-sec-group.tf
+++ lib/aws/bootstrap-ec2/ec2-sec-group.tf
@@ -13,15 +13,22 @@ resource "aws_security_group" "ec2_cluster" {
   tags = local.tags_ec2
 }
 
-# OPTIONAL: Allow inbound traffic from your local workstation external IP
-#           to the Kubernetes. You will need to replace A.B.C.D below with
-#           your real IP. Services like icanhazip.com can help you find this.
-resource "aws_security_group_rule" "cluster_ingress_workstation_https" {
-  cidr_blocks       = var.ec2_access_cidr_blocks
-  description       = "Allow workstation to communicate with the cluster API Server"
+resource "aws_security_group_rule" "https" {
+  cidr_blocks       = "0.0.0.0/0"
+  description       = "HTTPS connectivity"
   from_port         = 443
   protocol          = "tcp"
   security_group_id = aws_security_group.ec2_cluster.id
   to_port           = 443
   type              = "ingress"
+}
+
+resource "aws_security_group_rule" "ssh" {
+  cidr_blocks       = "0.0.0.0/0"
+  description       = "SSH remote access"
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.ec2_cluster.id
+  to_port           = 22
+  type              = "ssh"
 }
