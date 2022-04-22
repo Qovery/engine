5e0739586bf6239a1513d6f96430fd9936ef91a4
Romaric Philogene <evoxmusic@gmail.com>
2022-04-22 21:06:25 +0200

    wip: refactor according to Benjamin C. feedback

---
+++ src/cloud_provider/aws/kubernetes/ec2.rs
@@ -0,0 +1,390 @@
+use crate::cloud_provider;
+use crate::cloud_provider::aws::kubernetes;
+use crate::cloud_provider::aws::kubernetes::Options;
+use crate::cloud_provider::aws::regions::{AwsRegion, AwsZones};
+use crate::cloud_provider::environment::Environment;
+use crate::cloud_provider::kubernetes::{send_progress_on_long_task, Kind, Kubernetes, KubernetesUpgradeStatus};
+use crate::cloud_provider::utilities::print_action;
+use crate::cloud_provider::CloudProvider;
+use crate::dns_provider::DnsProvider;
+use crate::errors::EngineError;
+use crate::events::{EnvironmentStep, InfrastructureStep, Stage};
+use crate::io_models::{Action, Context, Listen, Listener, Listeners};
+use crate::logger::Logger;
+use crate::object_storage::s3::S3;
+use crate::object_storage::ObjectStorage;
+use function_name::named;
+use std::borrow::Borrow;
+use std::sync::Arc;
+
+pub struct EC2 {
+    context: Context,
+    id: String,
+    long_id: uuid::Uuid,
+    name: String,
+    version: String,
+    region: AwsRegion,
+    zones: Vec<AwsZones>,
+    cloud_provider: Arc<Box<dyn CloudProvider>>,
+    dns_provider: Arc<Box<dyn DnsProvider>>,
+    s3: S3,
+    template_directory: String,
+    options: Options,
+    listeners: Listeners,
+    logger: Box<dyn Logger>,
+}
+
+impl EC2 {
+    pub fn new(
+        context: Context,
+        id: &str,
+        long_id: uuid::Uuid,
+        name: &str,
+        version: &str,
+        region: AwsRegion,
+        zones: Vec<String>,
+        cloud_provider: Arc<Box<dyn CloudProvider>>,
+        dns_provider: Arc<Box<dyn DnsProvider>>,
+        options: Options,
+        logger: Box<dyn Logger>,
+    ) -> Result<Self, EngineError> {
+        let event_details = kubernetes::event_details(&cloud_provider, id, name, &region, &context);
+        let template_directory = format!("{}/aws-ec2/bootstrap", context.lib_root_dir());
+
+        let aws_zones = kubernetes::aws_zones(zones, &region, &event_details)?;
+        let s3 = kubernetes::s3(&context, &region, &**cloud_provider);
+
+        // copy listeners from CloudProvider
+        let listeners = cloud_provider.listeners().clone();
+        Ok(EC2 {
+            context,
+            id: id.to_string(),
+            long_id,
+            name: name.to_string(),
+            version: version.to_string(),
+            region,
+            zones: aws_zones,
+            cloud_provider,
+            dns_provider,
+            s3,
+            options,
+            template_directory,
+            logger,
+            listeners,
+        })
+    }
+
+    fn cloud_provider_name(&self) -> &str {
+        "aws"
+    }
+
+    fn struct_name(&self) -> &str {
+        "kubernetes"
+    }
+}
+
+impl Kubernetes for EC2 {
+    fn context(&self) -> &Context {
+        &self.context
+    }
+
+    fn kind(&self) -> Kind {
+        Kind::Ec2
+    }
+
+    fn id(&self) -> &str {
+        self.id.as_str()
+    }
+
+    fn name(&self) -> &str {
+        self.name.as_str()
+    }
+
+    fn version(&self) -> &str {
+        self.version.as_str()
+    }
+
+    fn region(&self) -> String {
+        self.region.to_aws_format()
+    }
+
+    fn zone(&self) -> &str {
+        ""
+    }
+
+    fn aws_zones(&self) -> Option<Vec<AwsZones>> {
+        Some(self.zones.clone())
+    }
+
+    fn cloud_provider(&self) -> &dyn CloudProvider {
+        (*self.cloud_provider).borrow()
+    }
+
+    fn dns_provider(&self) -> &dyn DnsProvider {
+        (*self.dns_provider).borrow()
+    }
+
+    fn logger(&self) -> &dyn Logger {
+        self.logger.borrow()
+    }
+
+    fn config_file_store(&self) -> &dyn ObjectStorage {
+        &self.s3
+    }
+
+    fn is_valid(&self) -> Result<(), EngineError> {
+        Ok(())
+    }
+
+    #[named]
+    fn on_create(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Create));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Create, || {
+            kubernetes::create(
+                self,
+                self.long_id,
+                self.template_directory.as_str(),
+                &self.zones,
+                &vec![],
+                &self.options,
+            )
+        })
+    }
+
+    #[named]
+    fn on_create_error(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Create));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Create, || kubernetes::create_error(self))
+    }
+
+    fn upgrade_with_status(&self, _kubernetes_upgrade_status: KubernetesUpgradeStatus) -> Result<(), EngineError> {
+        // TODO
+        Ok(())
+    }
+
+    #[named]
+    fn on_upgrade(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Upgrade));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Create, || self.upgrade())
+    }
+
+    #[named]
+    fn on_upgrade_error(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Upgrade));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Create, || kubernetes::upgrade_error(self))
+    }
+
+    #[named]
+    fn on_downgrade(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Downgrade));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Create, kubernetes::downgrade)
+    }
+
+    #[named]
+    fn on_downgrade_error(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Downgrade));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Create, || kubernetes::downgrade_error(self))
+    }
+
+    #[named]
+    fn on_pause(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Pause));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Pause, || {
+            kubernetes::pause(self, self.template_directory.as_str(), &self.zones, &vec![], &self.options)
+        })
+    }
+
+    #[named]
+    fn on_pause_error(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Pause));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Pause, || kubernetes::pause_error(self))
+    }
+
+    #[named]
+    fn on_delete(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Delete));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Delete, || {
+            kubernetes::delete(self, self.template_directory.as_str(), &self.zones, &vec![], &self.options)
+        })
+    }
+
+    #[named]
+    fn on_delete_error(&self) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Infrastructure(InfrastructureStep::Delete));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        send_progress_on_long_task(self, Action::Delete, || kubernetes::delete_error(self))
+    }
+
+    #[named]
+    fn deploy_environment(&self, environment: &Environment) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Environment(EnvironmentStep::Deploy));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details.clone(),
+            self.logger(),
+        );
+        cloud_provider::kubernetes::deploy_environment(self, environment, event_details, self.logger())
+    }
+
+    #[named]
+    fn deploy_environment_error(&self, environment: &Environment) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Environment(EnvironmentStep::Deploy));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details.clone(),
+            self.logger(),
+        );
+        cloud_provider::kubernetes::deploy_environment_error(self, environment, event_details, self.logger())
+    }
+
+    #[named]
+    fn pause_environment(&self, environment: &Environment) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Environment(EnvironmentStep::Pause));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details.clone(),
+            self.logger(),
+        );
+        cloud_provider::kubernetes::pause_environment(self, environment, event_details, self.logger())
+    }
+
+    #[named]
+    fn pause_environment_error(&self, _environment: &Environment) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Environment(EnvironmentStep::Pause));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        Ok(())
+    }
+
+    #[named]
+    fn delete_environment(&self, environment: &Environment) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Environment(EnvironmentStep::Delete));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details.clone(),
+            self.logger(),
+        );
+        cloud_provider::kubernetes::delete_environment(self, environment, event_details, self.logger())
+    }
+
+    #[named]
+    fn delete_environment_error(&self, _environment: &Environment) -> Result<(), EngineError> {
+        let event_details = self.get_event_details(Stage::Environment(EnvironmentStep::Delete));
+        print_action(
+            self.cloud_provider_name(),
+            self.struct_name(),
+            function_name!(),
+            self.name(),
+            event_details,
+            self.logger(),
+        );
+        Ok(())
+    }
+}
+
+impl Listen for EC2 {
+    fn listeners(&self) -> &Listeners {
+        &self.listeners
+    }
+
+    fn add_listener(&mut self, listener: Listener) {
+        self.listeners.push(listener);
+    }
+}
