95f3f15fbf2483dc245a3d83ff247baed34cc76a
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2022-04-28 18:17:45 +0200

    fix: setup proper tf values for ec2

--- lib/aws-ec2/bootstrap/qovery-tf-config.j2.tf
+++ lib/aws-ec2/bootstrap/qovery-tf-config.j2.tf
@@ -1,6 +1,15 @@
 locals {
   qovery_tf_config = <<TF_CONFIG
 {
+  "aws_iam_eks_user_mapper_key": "",
+  "aws_iam_eks_user_mapper_secret": "",
+  "aws_iam_cluster_autoscaler_key": "",
+  "aws_iam_cluster_autoscaler_secret": "",
+  "aws_iam_cloudwatch_key": "",
+  "aws_iam_cloudwatch_secret": "",
+  "loki_storage_config_aws_s3": "",
+  "aws_iam_loki_storage_key": "",
+  "aws_iam_loki_storage_secret": ""
 }
 TF_CONFIG
 }
