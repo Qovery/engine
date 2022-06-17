eda187af163b11e004599ffa343b6817fdd5c1b3
enzo <eradnai@qovery.com>
2022-06-17 14:54:43 +0200

    chore: upgrade kubernetes test version

--- lib-engine/test_utilities/src/aws.rs
+++ lib-engine/test_utilities/src/aws.rs
@@ -26,7 +26,7 @@ use crate::utilities::{build_platform_local_docker, FuncTestsSecrets};
 pub const AWS_REGION_FOR_S3: AwsRegion = AwsRegion::EuWest3;
 pub const AWS_TEST_REGION: AwsRegion = AwsRegion::EuWest3;
 pub const AWS_KUBERNETES_MAJOR_VERSION: u8 = 1;
-pub const AWS_KUBERNETES_MINOR_VERSION: u8 = 20;
+pub const AWS_KUBERNETES_MINOR_VERSION: u8 = 21;
 pub const AWS_KUBERNETES_VERSION: &str = formatcp!("{}.{}", AWS_KUBERNETES_MAJOR_VERSION, AWS_KUBERNETES_MINOR_VERSION);
 pub const AWS_DATABASE_INSTANCE_TYPE: &str = "db.t3.micro";
 pub const AWS_DATABASE_DISK_TYPE: &str = "gp2";
