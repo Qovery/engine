0774d758cd1ff9aff664b3da6bc2bfa1540c9aa9
Romain GERARD <erebe@erebe.eu>
2022-07-04 16:39:38 +0200

    feat: [ENG-1250] add rds config for user provided network topology

--- lib/aws/bootstrap/rds-user-network.j2.tf
+++ lib/aws/bootstrap/rds-user-network.j2.tf
@@ -1,3 +1,5 @@
+{%- if user_provided_network -%}
+
 data "aws_iam_policy_document" "rds_enhanced_monitoring" {
   statement {
     actions = [
@@ -22,62 +24,77 @@ locals {
   )
 }
 
+
+# Network is provided by the user
+variable "rds_subnets_zone_a_ids" {
+  type    = list(string)
+  default = [
+    {%- for id in rds_subnets_zone_a_ids -%}
+    "{{ id }}",
+    {%- endfor -%}
+  ]
+}
+
+variable "rds_subnets_zone_b_ids" {
+  type    = list(string)
+  default = [
+    {%- for id in rds_subnets_zone_b_ids -%}
+    "{{ id }}",
+    {%- endfor -%}
+  ]
+}
+
+variable "rds_subnets_zone_c_ids" {
+  type    = list(string)
+  default = [
+    {%- for id in rds_subnets_zone_c_ids -%}
+    "{{ id }}",
+    {%- endfor -%}
+  ]
+}
+
+
 # Network
-resource "aws_subnet" "rds_zone_a" {
-  count = length(var.rds_subnets_zone_a)
-
-  availability_zone = var.aws_availability_zones[0]
-  cidr_block        = var.rds_subnets_zone_a[count.index]
-  vpc_id            = aws_vpc.eks.id
-
-  tags = local.tags_rds
+data "aws_subnet" "rds_zone_a" {
+  count = length(var.rds_subnets_zone_a_ids)
+  id    = var.rds_subnets_zone_a_ids[count.index]
 }
 
-resource "aws_subnet" "rds_zone_b" {
-  count = length(var.rds_subnets_zone_b)
-
-  availability_zone = var.aws_availability_zones[1]
-  cidr_block        = var.rds_subnets_zone_b[count.index]
-  vpc_id            = aws_vpc.eks.id
-
-  tags = local.tags_rds
+data "aws_subnet" "rds_zone_b" {
+  count = length(var.rds_subnets_zone_b_ids)
+  id    = var.rds_subnets_zone_b_ids[count.index]
 }
 
-resource "aws_subnet" "rds_zone_c" {
-  count = length(var.rds_subnets_zone_c)
-
-  availability_zone = var.aws_availability_zones[2]
-  cidr_block        = var.rds_subnets_zone_c[count.index]
-  vpc_id            = aws_vpc.eks.id
-
-  tags = local.tags_rds
+data "aws_subnet" "rds_zone_c" {
+  count = length(var.rds_subnets_zone_c_ids)
+  id    = var.rds_subnets_zone_c_ids[count.index]
 }
 
 resource "aws_route_table_association" "rds_cluster_zone_a" {
-  count = length(var.rds_subnets_zone_a)
+  count = length(var.rds_subnets_zone_a_ids)
 
-  subnet_id      = aws_subnet.rds_zone_a.*.id[count.index]
+  subnet_id      = data.aws_subnet.rds_zone_a.*.id[count.index]
   route_table_id = aws_route_table.eks_cluster.id
 }
 
 resource "aws_route_table_association" "rds_cluster_zone_b" {
-  count = length(var.rds_subnets_zone_b)
+  count = length(var.rds_subnets_zone_b_ids)
 
-  subnet_id      = aws_subnet.rds_zone_b.*.id[count.index]
+  subnet_id      = data.aws_subnet.rds_zone_b.*.id[count.index]
   route_table_id = aws_route_table.eks_cluster.id
 }
 
 resource "aws_route_table_association" "rds_cluster_zone_c" {
-  count = length(var.rds_subnets_zone_c)
+  count = length(var.rds_subnets_zone_c_ids)
 
-  subnet_id      = aws_subnet.rds_zone_c.*.id[count.index]
+  subnet_id      = data.aws_subnet.rds_zone_c.*.id[count.index]
   route_table_id = aws_route_table.eks_cluster.id
 }
 
 resource "aws_db_subnet_group" "rds" {
   description = "RDS linked to ${var.kubernetes_cluster_id}"
   name = aws_vpc.eks.id
-  subnet_ids = flatten([aws_subnet.rds_zone_a.*.id, aws_subnet.rds_zone_b.*.id, aws_subnet.rds_zone_c.*.id])
+  subnet_ids = flatten([data.aws_subnet.rds_zone_a.*.id, data.aws_subnet.rds_zone_b.*.id, data.aws_subnet.rds_zone_c.*.id])
 
   tags = local.tags_rds
 }
@@ -116,3 +133,5 @@ resource "aws_security_group_rule" "mysql_remote_access" {
   to_port           = 3306
   type              = "ingress"
 }
+
+{%- endif -%}
