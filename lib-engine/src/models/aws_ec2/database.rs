d67e17692726528c5df0ba50d792e051290a9217
enzo <eradnai@qovery.com>
2022-07-08 15:42:39 +0200

    fix: enable cpu burst for ec2

--- lib-engine/src/models/aws_ec2/database.rs
+++ lib-engine/src/models/aws_ec2/database.rs
@@ -13,6 +13,7 @@ use crate::models::database::{
 };
 
 use crate::models::types::{AWSEc2, ToTeraContext};
+use crate::unit_conversion::cpu_string_to_float;
 use tera::Context as TeraContext;
 
 /////////////////////////////////////////////////////////////////
@@ -29,6 +30,26 @@ impl DatabaseType<AWSEc2, Container> for PostgresSQL {
     fn db_type() -> service::DatabaseType {
         service::DatabaseType::PostgreSQL
     }
+
+    fn cpu_validate(desired_cpu: String) -> String {
+        // todo: update core side to avoid passing String and keep u32 #ENG-1277
+        let cpu_size = cpu_string_to_float(desired_cpu.clone());
+        if cpu_size < 0.25 {
+            // todo: return an error instead?
+            "250m".to_string()
+        } else {
+            desired_cpu
+        }
+    }
+
+    fn memory_validate(desired_memory: u32) -> u32 {
+        if desired_memory < 100 {
+            // todo: return an error instead?
+            100
+        } else {
+            desired_memory
+        }
+    }
 }
 
 impl DatabaseType<AWSEc2, Container> for MySQL {
@@ -43,6 +64,38 @@ impl DatabaseType<AWSEc2, Container> for MySQL {
     fn db_type() -> service::DatabaseType {
         service::DatabaseType::MySQL
     }
+
+    fn cpu_validate(desired_cpu: String) -> String {
+        // todo: update core side to avoid passing String and keep u32 #ENG-1277
+        let cpu_size = cpu_string_to_float(desired_cpu.clone());
+        if cpu_size < 0.25 {
+            // todo: return an error instead?
+            "250m".to_string()
+        } else {
+            desired_cpu
+        }
+    }
+
+    // lower than 500m, it's too long to start and fails. Better to allow cpu overcommit than growing init boot value
+    fn cpu_burst_value(desired_cpu: String) -> String {
+        // todo: update core side to avoid passing String and keep u32 #ENG-1277
+        let cpu_size = cpu_string_to_float(desired_cpu.clone());
+        if cpu_size < 0.5 {
+            // todo: return an error instead?
+            "500m".to_string()
+        } else {
+            desired_cpu
+        }
+    }
+
+    fn memory_validate(desired_memory: u32) -> u32 {
+        if desired_memory < 100 {
+            // todo: return an error instead?
+            100
+        } else {
+            desired_memory
+        }
+    }
 }
 
 impl DatabaseType<AWSEc2, Container> for Redis {
@@ -54,9 +107,28 @@ impl DatabaseType<AWSEc2, Container> for Redis {
     fn lib_directory_name() -> &'static str {
         "redis"
     }
-
     fn db_type() -> service::DatabaseType {
         service::DatabaseType::Redis
+    }
+
+    fn cpu_validate(desired_cpu: String) -> String {
+        // todo: update core side to avoid passing String and keep u32 #ENG-1277
+        let cpu_size = cpu_string_to_float(desired_cpu.clone());
+        if cpu_size < 0.25 {
+            // todo: return an error instead?
+            "250m".to_string()
+        } else {
+            desired_cpu
+        }
+    }
+
+    fn memory_validate(desired_memory: u32) -> u32 {
+        if desired_memory < 100 {
+            // todo: return an error instead?
+            100
+        } else {
+            desired_memory
+        }
     }
 }
 
@@ -64,15 +136,33 @@ impl DatabaseType<AWSEc2, Container> for MongoDB {
     type DatabaseOptions = DatabaseOptions;
 
     fn short_name() -> &'static str {
-        "Redis"
+        "MongoDb"
     }
-
     fn lib_directory_name() -> &'static str {
         "mongodb"
     }
-
     fn db_type() -> service::DatabaseType {
         service::DatabaseType::MongoDB
+    }
+
+    fn cpu_validate(desired_cpu: String) -> String {
+        // todo: update core side to avoid passing String and keep u32 #ENG-1277
+        let cpu_size = cpu_string_to_float(desired_cpu.clone());
+        if cpu_size < 0.25 {
+            // todo: return an error instead?
+            "250m".to_string()
+        } else {
+            desired_cpu
+        }
+    }
+
+    fn memory_validate(desired_memory: u32) -> u32 {
+        if desired_memory < 256 {
+            // todo: return an error instead?
+            256
+        } else {
+            desired_memory
+        }
     }
 }
 
@@ -102,7 +192,6 @@ impl DatabaseType<AWSEc2, Managed> for MySQL {
     fn lib_directory_name() -> &'static str {
         "mysql"
     }
-
     fn db_type() -> service::DatabaseType {
         service::DatabaseType::MySQL
     }
@@ -117,7 +206,6 @@ impl DatabaseType<AWSEc2, Managed> for Redis {
     fn lib_directory_name() -> &'static str {
         "redis"
     }
-
     fn db_type() -> service::DatabaseType {
         service::DatabaseType::Redis
     }
