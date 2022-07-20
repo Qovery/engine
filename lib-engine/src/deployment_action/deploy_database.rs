f2a9f99ac77a017cc30a8d5fb38bdee7126f0f8d
Romain GERARD <erebe@erebe.eu>
2022-07-20 16:26:26 +0200

    feat: Add pause for AWS managed databases

--- lib-engine/src/deployment_action/deploy_database.rs
+++ lib-engine/src/deployment_action/deploy_database.rs
@@ -2,16 +2,18 @@ use crate::cloud_provider::service::{
     delete_managed_stateful_service, delete_pending_service, deploy_managed_database_service, Action, Helm, Service,
 };
 use crate::cloud_provider::utilities::{check_domain_for, print_action};
-use crate::cloud_provider::DeploymentTarget;
+use crate::cloud_provider::Kind::Aws;
+use crate::cloud_provider::{service, DeploymentTarget};
+use crate::cmd::command::QoveryCommand;
 use crate::deployment_action::deploy_helm::HelmDeployment;
 use crate::deployment_action::pause_service::PauseServiceAction;
 use crate::deployment_action::DeploymentAction;
 use crate::deployment_report::database::reporter::DatabaseDeploymentReporter;
 use crate::deployment_report::execute_long_deployment;
-use crate::errors::EngineError;
+use crate::errors::{CommandError, EngineError};
 use crate::events::{EnvironmentStep, Stage};
 use crate::io_models::ListenersHelper;
-use crate::models::database::{Container, Database, DatabaseType, Managed};
+use crate::models::database::{Container, Database, DatabaseService, DatabaseType, Managed};
 use crate::models::types::{CloudProvider, ToTeraContext};
 use function_name::named;
 use std::path::PathBuf;
@@ -73,11 +75,56 @@ where
             T::db_type().to_string().as_str(),
             function_name!(),
             self.name(),
-            event_details,
+            event_details.clone(),
             self.logger(),
         );
 
-        execute_long_deployment(DatabaseDeploymentReporter::new(self, target, Action::Pause), || Ok(()))
+        execute_long_deployment(DatabaseDeploymentReporter::new(self, target, Action::Pause), || {
+            // We don't manage PAUSE for managed database elsewhere than for AWS
+            if target.kubernetes.cloud_provider().kind() != Aws {
+                return Ok(());
+            }
+
+            let mut output_stdout: Vec<String> = vec![];
+            let mut output_stderr: Vec<String> = vec![];
+            let ret = match self.db_type() {
+                service::DatabaseType::PostgreSQL | service::DatabaseType::MySQL => {
+                    // We use the fqdn_id as db identifier, why not id or name like everything else ¯\_(ツ)_/¯
+                    let mut cmd = QoveryCommand::new(
+                        "aws",
+                        &["rds", "stop-db-instance", "--db-instance-identifier", &self.fqdn_id],
+                        &target.kubernetes.cloud_provider().credentials_environment_variables(),
+                    );
+                    cmd.exec_with_output(&mut |line| output_stdout.push(line), &mut |line| output_stderr.push(line))
+                }
+                service::DatabaseType::MongoDB => {
+                    // We use the fqdn_id as db identifier, why not id or name like everything else ¯\_(ツ)_/¯
+                    let mut cmd = QoveryCommand::new(
+                        "aws",
+                        &["docdb", "stop-db-cluster", "--db-cluster-identifier", &self.fqdn_id],
+                        &target.kubernetes.cloud_provider().credentials_environment_variables(),
+                    );
+                    cmd.exec_with_output(&mut |line| output_stdout.push(line), &mut |line| output_stderr.push(line))
+                }
+                service::DatabaseType::Redis => {
+                    // can't pause elasticache
+                    Ok(())
+                }
+            };
+
+            output_stdout.extend(output_stderr);
+            if let Err(cmd_error) = ret {
+                Err(EngineError::new_cannot_pause_managed_database(
+                    event_details.clone(),
+                    CommandError::new_from_legacy_command_error(
+                        cmd_error,
+                        Some(output_stdout.join("\n").trim().to_string()),
+                    ),
+                ))
+            } else {
+                Ok(())
+            }
+        })
     }
 
     #[named]
