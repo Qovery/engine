b4f341987fa2eb879cc268a3772fd619c563151f
Romain GERARD <erebe@erebe.eu>
2022-07-20 08:53:18 +0200

    style: remove useless lock

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,19 +1,16 @@
 use lazy_static::lazy_static;
 use std::future::Future;
-use std::sync::RwLock;
 use tokio::runtime::{Builder, Runtime};
 
 lazy_static! {
-    static ref TOKIO_RUNTIME: RwLock<Runtime> = RwLock::new({
-        Builder::new_multi_thread()
-            .worker_threads(2)
-            .thread_name("tokio-engine-blocking")
-            .enable_all()
-            .build()
-            .unwrap()
-    });
+    static ref TOKIO_RUNTIME: Runtime = Builder::new_multi_thread()
+        .worker_threads(2)
+        .thread_name("tokio-engine-blocking")
+        .enable_all()
+        .build()
+        .unwrap();
 }
 
 pub fn block_on<F: Future>(future: F) -> F::Output {
-    TOKIO_RUNTIME.read().unwrap().block_on(future)
+    TOKIO_RUNTIME.block_on(future)
 }
