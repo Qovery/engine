42e46dfa91e054eba2d8e0bddcc66855eafbc1fd
Σrebe - Romain GERARD <erebe@erebe.eu>
2022-08-03 09:48:22 +0200

    style: Add message when deleting registry

--- src/deployment_action/deploy_application.rs
+++ src/deployment_action/deploy_application.rs
@@ -11,6 +11,7 @@ use crate::errors::EngineError;
 use crate::events::{EnvironmentStep, Stage};
 use crate::models::application::Application;
 use crate::models::types::{CloudProvider, ToTeraContext};
+use cmd_lib::log::log;
 use function_name::named;
 use std::path::PathBuf;
 use std::time::Duration;
@@ -95,11 +96,12 @@ where
             // FIXME: Delete pvc
 
             // Delete container repository created for this application
+            let logger = get_loggers(self, Action::Delete);
+            (logger.send_progress)(format!("🪓 Terminating container registry of the application"));
             if let Err(err) = target
                 .container_registry
                 .delete_repository(self.build().image.repository_name())
             {
-                let logger = get_loggers(self, Action::Delete);
                 let engine_err = EngineError::new_container_registry_delete_repository_error(
                     event_details.clone(),
                     self.build().image.repository_name().to_string(),
