975b607cffc85b7724dd2cb965138f0b4fa23103
Romain GERARD <erebe@erebe.eu>
2022-10-28 09:51:04 +0200

    fix: Better job report

--- src/deployment_report/job/reporter.rs
+++ src/deployment_report/job/reporter.rs
@@ -197,12 +197,12 @@ impl<T: Send + Sync> DeploymentReporter for JobDeploymentReporter<T> {
             self.logger.send_error(EngineError::new_engine_error(
                 error.clone(),
                 format!(r#"
-❌ {} failed to be executed in the given time frame due to `{}`.
+❌ {} failed to be executed in the given {} seconds due to `{}`.
 This most likely an issue with its configuration/code.
 Look at your logs in order to understand what went wrong or increase its max duration timeout
 
 ⛑ Need Help ? Please consult our FAQ to troubleshoot your deployment https://hub.qovery.com/docs/using-qovery/troubleshoot/ and visit the forum https://discuss.qovery.com/
-                "#, self.job_type, job_failure_message.unwrap_or_default()).trim().to_string(),
+                "#, self.job_type, self.max_duraction.as_secs(),  job_failure_message.unwrap_or_default()).trim().to_string(),
                 None,
             ));
         } else {
