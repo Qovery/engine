a8311361eecaa360793541226c81b376f8c9ae46
benjaminch <bchastanier@qovery.com>
2022-12-16 17:24:30 +0000

    revert: dedicated struct for nginx-ingress chart

--- src/cloud_provider/models.rs
+++ src/cloud_provider/models.rs
@@ -162,11 +162,6 @@ impl Display for KubernetesMemoryResourceUnit {
     }
 }
 
-pub trait IngressLoadBalancerType {
-    fn annotation_key(&self) -> String;
-    fn annotation_value(&self) -> String;
-}
-
 #[cfg(test)]
 mod tests {
     use crate::cloud_provider::models::{KubernetesCpuResourceUnit, KubernetesMemoryResourceUnit};
