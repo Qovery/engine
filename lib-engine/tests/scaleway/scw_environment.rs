a4a10d2aba1c5e8bcb2dfef6d7eb7dcf02ae490f
Σrebe - Romain GERARD <erebe@erebe.eu>
2023-01-18 09:40:16 +0100

    fix: scaleway test

--- lib-engine/tests/scaleway/scw_environment.rs
+++ lib-engine/tests/scaleway/scw_environment.rs
@@ -1470,7 +1470,7 @@ fn deploy_cronjob_on_scw_kapsule() {
 
         environment.applications = vec![];
         environment.jobs = vec![Job {
-            long_id: Uuid::default(),
+            long_id: Uuid::new_v4(),
             name: "job test #####||||*_-(".to_string(),
             action: Action::Create,
             schedule: JobSchedule::Cron {
