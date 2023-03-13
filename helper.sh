b96c2ded829680901a2899fe21339a0ccf0d1311
Romain GERARD <erebe@erebe.eu>
2023-03-13 16:53:03 +0100

    chore: bump ram limit to match node size

--- helper.sh
+++ helper.sh
@@ -296,7 +296,7 @@ function deploy_engines_envs() { ## Release GA to prod
   --set-string \
 environmentVariables.BUILDER_KUBE_ENABLED="true",\
 environmentVariables.BUILDER_CPU_REQUEST="3",\
-environmentVariables.BUILDER_CPU_LIMIT="4"\
+environmentVariables.BUILDER_CPU_LIMIT="4",\
 environmentVariables.BUILDER_MEMORY_REQUEST_GIB="10",\
 environmentVariables.BUILDER_MEMORY_LIMIT_GIB="10"\
   --set image.tag="$tag",\
