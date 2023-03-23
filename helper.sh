bfdd4bd3f6160aae6b9dad332773bc78a3140442
Romain GERARD <erebe@erebe.eu>
2023-03-23 15:29:13 +0100

    chore: reduce engine resources

--- helper.sh
+++ helper.sh
@@ -208,10 +208,10 @@ autoscaler.averageValue="0.9",\
 overprovisionning.enabled="true",\
 overprovisionning.replicas="9",\
 engineResources.limits.cpu="1",\
-engineResources.limits.memory="3.9Gi",\
+engineResources.limits.memory="1Gi",\
 engineResources.limits.ephemeral-storage="20Gi",\
 engineResources.requests.cpu="300m",\
-engineResources.requests.memory="3.9Gi",\
+engineResources.requests.memory="1Gi",\
 engineResources.requests.ephemeral-storage="20Gi"
 }
 
