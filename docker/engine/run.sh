cb4748eb6f91f0c7789a5554f5e705db2ff18e90
Σrebe - Romain GERARD <erebe@erebe.eu>
2023-03-24 15:20:27 +0100

    fix engine deployment

--- docker/engine/run.sh
+++ docker/engine/run.sh
@@ -2,8 +2,8 @@
 
 
 echo "Cleaning workspace ${WORKSPACE_ROOT_DIR:=/home/qovery}/.qovery-workspace"
-rm -rf ${WORKSPACE_ROOT_DIR}/.qovery-workspace 
 mkdir ${WORKSPACE_ROOT_DIR}/.qovery-workspace 
+rm -rf ${WORKSPACE_ROOT_DIR}/.qovery-workspace/*
 
 git lfs install
 git config --global credential.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASSWORD}"; }; f'
