f9d5b17462b1e149e110b88b8a61e33389535b12
Romain GERARD <erebe@erebe.eu>
2023-03-30 16:57:12 +0200

    chore: use guard to reduce chance of unparking while not finished

--- lib-engine/src/engine_task/environment_task.rs
+++ lib-engine/src/engine_task/environment_task.rs
@@ -574,11 +574,10 @@ impl BuilderThreadPool {
 
                 // We have a slot to run a new thread, so start a new build
                 let th = thread::Builder::new().name("builder".to_string()).spawn_scoped(scope, {
-                    let current_thread = current_thread.clone();
+                    let current_thread = &current_thread;
                     move || {
-                        let ret = task();
-                        current_thread.unpark();
-                        ret
+                        let _guard = scopeguard::guard((), |_| current_thread.unpark());
+                        task()
                     }
                 });
                 active_threads.push_back(th.unwrap());
