40f2d968beb27d1fae9e1530c592b496867f0a35
Qovery <noreply@qovery.com>
2023-10-25 09:01:42 +0000

    bump 10/25/23 09:01:42

--- lib/scaleway/bootstrap/ks-workers-nodes.j2.tf
+++ lib/scaleway/bootstrap/ks-workers-nodes.j2.tf
@@ -1,8 +1,10 @@
 {% for scw_ks_worker_node in scw_ks_worker_nodes %}
 resource "scaleway_k8s_pool" "kubernetes_cluster_workers_{{ loop.index }}" {
   cluster_id    = scaleway_k8s_cluster.kubernetes_cluster.id
-  name          = "${var.kubernetes_cluster_id}_{{ scw_ks_worker_node.instance_type }}_{{ loop.index }}"
   node_type     = "{{ scw_ks_worker_node.instance_type }}"
+
+  # name to include instance type and disk size, because such changes requires creating a new pool and name should be unique
+  name          = "${var.kubernetes_cluster_id}_{{ scw_ks_worker_node.instance_type }}_{{ scw_ks_worker_node.disk_size_in_gib }}_{{ loop.index }}"
 
   region        = var.region
   zone          = var.zone
@@ -14,6 +16,8 @@ resource "scaleway_k8s_pool" "kubernetes_cluster_workers_{{ loop.index }}" {
   min_size            = "{{ scw_ks_worker_node.min_nodes }}"
   max_size            = "{{ scw_ks_worker_node.max_nodes }}"
   wait_for_pool_ready = false
+
+  root_volume_size_in_gb = {{ scw_ks_worker_node.disk_size_in_gib }}
 
   timeouts {
     create = "30m"
