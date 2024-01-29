8c97151485133107af677271de76d382cbbd0b3e
Melvin Zottola <mzottola@qovery.com>
2024-01-29 11:01:12 +0100

    chore: Prepare 1.28

--- src/cloud_provider/aws/kubernetes/addons/aws_ebs_csi_addon.rs
+++ src/cloud_provider/aws/kubernetes/addons/aws_ebs_csi_addon.rs
@@ -19,7 +19,7 @@ impl AwsEbsCsiAddon {
                 KubernetesVersion::V1_25 { .. } => "v1.19.0-eksbuild.2",
                 KubernetesVersion::V1_26 { .. } => "v1.20.0-eksbuild.1",
                 KubernetesVersion::V1_27 { .. } => "v1.26.1-eksbuild.1",
-                KubernetesVersion::V1_28 { .. } => "v1.26.1-eksbuild.1",
+                KubernetesVersion::V1_28 { .. } => "v1.27.0-eksbuild.1",
             }
             .to_string(),
         }
