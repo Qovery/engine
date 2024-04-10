5afb377fcb4bf98a0b2163716e073949c3728c91
BenjaminCh <bchastanier@qovery.com>
2024-04-10 12:46:23 +0200

    fix: GCP loki service account role

--- lib/gcp/bootstrap/sa-loki.j2.tf
+++ lib/gcp/bootstrap/sa-loki.j2.tf
@@ -6,19 +6,17 @@ locals {
 
 resource "google_service_account" "loki_service_account" {
   account_id   = local.loki_service_account_name
-  display_name = "Service account for Loki"
+  display_name = "Service account for Loki for cluster ${var.kubernetes_cluster_name}"
   project      = var.project_id
   # Putting tags as JSON in description since services accounts don't support tags
   description  = jsonencode(local.minimal_tags_common) # limited length to 256 chars
 }
 
-resource "google_project_iam_binding" "project" {
+resource "google_project_iam_member" "project" {
   project = var.project_id
   role    = "roles/storage.objectAdmin"
 
-  members = [
-    "serviceAccount:${resource.google_service_account.loki_service_account.email}"
-  ]
+  member = "serviceAccount:${resource.google_service_account.loki_service_account.email}"
 }
 
 resource "google_service_account_iam_binding" "loki-workload-identity" {
