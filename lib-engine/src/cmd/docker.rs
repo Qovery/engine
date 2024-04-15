2bdf60445bc85091b74295150d4f91e9683d2461
Σrebe - Romain GERARD <erebe@erebe.eu>
2024-04-15 10:56:51 +0200

    Add delay when launching builder to avoid network policies timeout

--- lib-engine/src/cmd/docker.rs
+++ lib-engine/src/cmd/docker.rs
@@ -406,6 +406,14 @@ impl Docker {
                     )?;
                 }
 
+                // HACK: https://github.com/aws/aws-network-policy-agent/issues/245
+                // There is a race condition in the network policy agent that can can cause timeout if we start too early to send traffic
+                // Delay a bit to allow bpf probe to be attached to the pod, before starting build that send traffic
+                debug!(
+                    "Builder ready but waiting 5sec due to https://github.com/aws/aws-network-policy-agent/issues/245"
+                );
+                std::thread::sleep(Duration::from_secs(5));
+
                 Ok(build_handle)
             }
         }
