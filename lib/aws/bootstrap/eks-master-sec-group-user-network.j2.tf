2800a6525cafa9ac04a76881eab2b780245b4bdb
Σrebe - Romain GERARD <erebe@erebe.eu>
2024-10-08 15:49:11 +0200

    chore(aws): remove un-needed security groups

--- lib/aws/bootstrap/eks-master-sec-group-user-network.j2.tf
+++ lib/aws/bootstrap/eks-master-sec-group-user-network.j2.tf
@@ -2,18 +2,10 @@
 
 resource "aws_security_group" "eks_cluster" {
   name        = "qovery-eks-${var.kubernetes_cluster_id}"
-  description = "Cluster communication with worker nodes"
+  description = "Cluster communication from control plane to worker nodes"
   vpc_id      = data.aws_vpc.eks.id
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
