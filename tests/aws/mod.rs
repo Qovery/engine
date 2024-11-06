a8f83e308ead725fd8c53bb427074a9973e4d583
Pierre Mavro <pmavro@qovery.com>
2024-11-06 11:02:36 +0100

    fix: remove ec2 tests

--- tests/aws/mod.rs
+++ tests/aws/mod.rs
@@ -1,9 +1,6 @@
 mod aws_databases;
-mod aws_ec2_whole_enchilada_databases;
 mod aws_ecr;
 mod aws_environment;
-mod aws_environment_ec2;
 mod aws_kubernetes;
-mod aws_kubernetes_ec2;
 mod aws_s3;
 mod aws_whole_enchilada;
