e3c4058c68f02bd2e24acfd4316bfa4e9562dbd1
Romain GERARD <rgerard@qovery.com>
2024-12-12 14:37:43 +0000

    chore: remove vault from infra deployments

--- lib-engine/lib/gcp/bootstrap/terraform/qovery-tf-output.j2.tf
+++ lib-engine/lib/gcp/bootstrap/terraform/qovery-tf-output.j2.tf
@@ -1,5 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 {%- if log_history_enabled %}
 output "loki_logging_service_account_email" { value = resource.google_service_account.loki_service_account.email }
 {%- endif %}
