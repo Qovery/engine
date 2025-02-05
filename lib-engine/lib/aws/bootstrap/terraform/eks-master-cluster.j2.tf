70b3007eb4460ac66d05eee170a25bbae528227c
Romain GERARD <rgerard@qovery.com>
2025-02-05 11:53:24 +0000

    fix(aws): Add correct permission to access eks cluster for role

--- lib-engine/lib/aws/bootstrap/terraform/eks-master-cluster.j2.tf
+++ lib-engine/lib/aws/bootstrap/terraform/eks-master-cluster.j2.tf
@@ -52,6 +52,10 @@ resource "aws_eks_cluster" "eks_cluster" {
       support_type = "STANDARD"
   }
 
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
 {% if aws_eks_encrypt_secrets_kms_key_arn -%}
   encryption_config {
       provider {
