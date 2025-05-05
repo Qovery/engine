4fafb67b312ccde239fc7e1cdb705bac8e6f4c57
benjaminch <bchastanier@qovery.com>
2025-05-05 14:08:06 +0000

    chore(ci): bump local k3s version to 1.31

--- helper.sh
+++ helper.sh
@@ -297,7 +297,7 @@ function test_local_stack() {
     # We can't use our ECR public repo yet :/
     k3d cluster create -a 0 \
         --agents 0 \
-        --image rancher/k3s:v1.28.12-k3s1 \
+        --image rancher/k3s:v1.31.7-k3s1 \
         --no-lb \
         --k3s-arg "--disable=traefik" \
         --wait $kube_cluster_name || k3d cluster start --wait $kube_cluster_name
