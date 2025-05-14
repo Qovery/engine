d456682c105289a6a720bacb7b9fcf5f706bb9c2
Fabien FLEUREAU <ffleureau@qovery.com>
2025-05-14 08:13:35 +0000

    fix: add support for mexico region by upgrading aws tf provider

--- lib-engine/lib/aws/bootstrap/terraform/tf-providers-aws.j2.tf
+++ lib-engine/lib/aws/bootstrap/terraform/tf-providers-aws.j2.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version    = "5.69.0"
+      version    = "5.84.0"
     }
     external = {
       source = "hashicorp/external"
