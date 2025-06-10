0b3f637cc3cf65e2a70e2b452775986d2cb78065
Pierre Gerbelot <pgerbelot@qovery.com>
2025-06-10 11:16:55 +0000

    feat(terraform): add cloud provider credential if none are provided by the user + improve logs

--- tests/azure/azure_environment.rs
+++ tests/azure/azure_environment.rs
@@ -2004,6 +2004,7 @@ fn azure_aks_deploy_terraform_service() {
             annotations_group_ids: btreeset! { annotations_group_id },
             labels_group_ids: btreeset! { labels_group_id },
             shared_image_feature_enabled: false,
+            terraform_credentials: None,
         }
     }
 
