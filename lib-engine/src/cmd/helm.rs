f3355b335daa6f414b45f7a6054bef8bfbc4e59b
bchastanier <bchastanier@qovery.com>
2025-06-17 10:02:03 +0200

    fix(ci): fix helm version test

--- lib-engine/src/cmd/helm.rs
+++ lib-engine/src/cmd/helm.rs
@@ -1770,7 +1770,7 @@ mod tests {
             &mut |_line| {},
             &CommandKiller::never(),
         );
-        assert!(output.contains("Version:\"v3.17.3-1\""));
+        assert!(output.contains("Version:\"v3.17.3\""));
     }
 
     #[test]
