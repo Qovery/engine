fc6a0c05ee00e7bd4c80912f011d9d59c1de89b7
bchastanier <bchastanier@qovery.com>
2025-06-30 22:11:59 +0200

    chore(aws): bump AWS TF provider version

--- lib/aws/bootstrap/terraform/tf-providers-aws.j2.tf
+++ lib/aws/bootstrap/terraform/tf-providers-aws.j2.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version    = "5.84.0"
+      version = "6.0.0"
     }
     external = {
       source = "hashicorp/external"
