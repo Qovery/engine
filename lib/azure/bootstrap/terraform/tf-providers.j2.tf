14be1d58a3bd693a10309f5a8988a5153b95efbf
bchastanier <bchastanier@qovery.com>
2025-10-08 15:30:23 +0200

    fix(QOV-1226): azure tf provider to not automatic resource provider registration step

--- lib/azure/bootstrap/terraform/tf-providers.j2.tf
+++ lib/azure/bootstrap/terraform/tf-providers.j2.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = "1.9.7"
-  
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 4.19"
     }
     azapi = {
-      source = "azure/azapi"
+      source  = "azure/azapi"
       version = ">= 2.2.0"
     }
     time = {
@@ -20,8 +20,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+  client_id                       = var.client_id
+  client_secret                   = var.client_secret
+  tenant_id                       = var.tenant_id
+  subscription_id                 = var.subscription_id
+  resource_provider_registrations = "none"
 }
