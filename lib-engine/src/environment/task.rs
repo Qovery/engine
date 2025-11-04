0336b024ae38d574e626ed857055a762108d5d3f
Σrebe - Romain GERARD <erebe@erebe.eu>
2025-11-04 16:29:52 +0100

    fix(deployment-logs): correctly take the last value of the image tag
    .build() may mut the image tag based on the ENV of the Dockerfile
    So display the image name after the build is made

--- lib-engine/src/environment/task.rs
+++ lib-engine/src/environment/task.rs
@@ -225,10 +225,10 @@ impl EnvironmentTask {
             Some(build) => build,
             None => return Ok(()), // this case should not happen as we filter on buildable services
         };
-        let image_name = build.image.full_image_name_with_tag();
 
         // If image already exists in the registry, skip the build
         if !option.force_build && cr_registry.image_exists(&build.image) {
+            let image_name = build.image.full_image_name_with_tag();
             let msg = format!("✅ Container image {image_name} already exists and ready to use");
             logger.send_success(msg);
             return Ok(());
@@ -265,6 +265,7 @@ impl EnvironmentTask {
 
         // Ok now everything is setup, we can try to build the app
         let build_result = build_platform.build(build, &logger, metrics_registry.clone(), abort);
+        let image_name = build.image.full_image_name_with_tag(); // .build() may have modified the image tag
         match build_result {
             Ok(_) => {
                 let msg = format!("✅ Container image {} is built and ready to use", &image_name);
