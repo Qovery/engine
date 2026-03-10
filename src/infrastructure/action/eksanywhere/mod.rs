5c6ef1f07b30ba0379c94dc855dc11fbcc2a9b7e
Pierre Gerbelot <pgerbelot@qovery.com>
2026-03-10 16:11:03 +0100

    feat(powens): add support for eksctl for eks anywhere cluster

--- src/infrastructure/action/eksanywhere/mod.rs
+++ src/infrastructure/action/eksanywhere/mod.rs
@@ -1,12 +1,13 @@
 mod cluster_install;
 mod helm_charts;
 
+use crate::cmd::command::{ExecutableCommand, QoveryCommand};
 use crate::errors::EngineError;
 use crate::events::InfrastructureStep;
 use crate::events::Stage::Infrastructure;
-use crate::infrastructure::action::InfrastructureAction;
 use crate::infrastructure::action::eksanywhere::cluster_install::install_eks_anywhere_charts;
 use crate::infrastructure::action::utils::mk_logger;
+use crate::infrastructure::action::{InfraLogger, InfrastructureAction};
 use crate::infrastructure::infrastructure_context::InfrastructureContext;
 use crate::infrastructure::models::cloud_provider::service::Action;
 use crate::infrastructure::models::kubernetes::eksanywhere::EksAnywhere;
@@ -19,6 +20,20 @@ impl InfrastructureAction for EksAnywhere {
         _has_been_upgraded: bool,
     ) -> Result<(), Box<EngineError>> {
         let logger = mk_logger(infra_ctx.kubernetes(), InfrastructureStep::Create);
+        let mut eksctl_version = String::new();
+        let mut cmd = QoveryCommand::new("eksctl", &["version"], &[]);
+        if cmd
+            .exec_with_output(&mut |line| eksctl_version.push_str(&line), &mut |line| {
+                warn!("Error while getting `eksctl` version: {}", line)
+            })
+            .is_err()
+            || eksctl_version.trim().is_empty()
+        {
+            logger.warn("Unable to get `eksctl` version using `eksctl version`.");
+        } else {
+            logger.info(format!("Using eksctl: {}", eksctl_version.trim()));
+        }
+
         send_progress_on_long_task(self, Action::Create, || install_eks_anywhere_charts(self, infra_ctx, logger))
     }
 
