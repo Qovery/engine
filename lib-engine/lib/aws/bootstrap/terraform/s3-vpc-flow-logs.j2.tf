a3615d08807b7ff44bcff8049514a6f027cc3c42
Melvin Zottola <mzottola@qovery.com>
2026-04-23 10:38:09 +0200

    chore: Apply SSE-C reco for aws s3 vpc flow logs

--- lib-engine/lib/aws/bootstrap/terraform/s3-vpc-flow-logs.j2.tf
+++ lib-engine/lib/aws/bootstrap/terraform/s3-vpc-flow-logs.j2.tf
@@ -66,11 +66,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "flow_logs_bucket_
   bucket = aws_s3_bucket.vpc_flow_logs.id
 
   rule {
-    blocked_encryption_types = ["NONE"]
+    blocked_encryption_types = ["SSE-C"]
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.s3_logs_kms_encryption.arn
+      sse_algorithm = "aws:kms"
     }
-    bucket_key_enabled = true
+    bucket_key_enabled = false
   }
 }
 
